1a88d34d38ce33568b1d26b8c964ba80f674c6a7
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-05-11 08:18:18 +0200

    Home index with simple stimulus example

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,9 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-
 console.log('Hello World from Webpacker')
+
+import 'stylesheets/application'
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
