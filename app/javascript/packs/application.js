2d6d1a9f8eaee4abf2232845b3e8a7041fc43bf4
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-05-17 13:34:08 +0200

    Make fontawesome 5 works well with turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,10 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("controllers", true, /.js$/)
 application.load(definitionsFromContext(context))
+
+document.addEventListener("turbolinks:before-render", function(event) {
+  FontAwesome.dom.i2svg({
+    node: event.data.newBody
+  });
+});
+
