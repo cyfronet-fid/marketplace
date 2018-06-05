5a613010e37ecc5b9d75c0316965ea4575ff2762
Jakub Sawicki <ymsawick@cyfronet.pl>
2018-06-05 15:33:27 +0200

    Create my profile page
    The page shows read-only user information, as fetched from check-in.
    Authorization for profile page is created, along with 401 error page.
    A link is added to the navbar, which is shown when user is logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ require "turbolinks"
 
 class ApplicationController < ActionController::Base
   include Turbolinks::Redirection
+
+  private
+
+  def authenticate_user!
+    render :file => "public/401", :status => :unauthorized, :layout => false unless current_user
+  end
 end
