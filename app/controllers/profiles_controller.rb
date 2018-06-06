6813732673143d2e59b87ec58de47c2178008d63
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-06-06 15:40:45 +0200

    Review fixes

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,14 +2,7 @@
 
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
 
   def show
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = current_user
-    end
 end
