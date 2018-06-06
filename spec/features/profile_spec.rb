f204f8729879517fcbf8c328aabe150cf5f01f51
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-06-06 15:28:22 +0200

    Unauthorized user redirects tests

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -5,13 +5,11 @@ require "rails_helper"
 RSpec.feature "Profile page" do
   include OmniauthHelper
 
-  scenario "User can see his profile" do
+  scenario "can be seen by authenticated user" do
     user = create(:user)
 
     checkin_sign_in_as(user)
 
-    expect(page.body).to have_selector("#navbarSupportedContent li.nav-item a.nav-link", text: "My profile")
-  
     click_link('My profile')
 
     expect(page.body).to have_text(user.first_name)
@@ -19,16 +17,10 @@ RSpec.feature "Profile page" do
     expect(page.body).to have_text(user.email)
   end
 
-  scenario "Unauthenticated user doesn't see navbar link to profile" do
+  scenario "link isn't visible to unauthenticated user" do
     visit root_path
     
-    expect(page.body).to have_no_selector("#navbarSupportedContent li.nav-item a.nav-link", text: "My profile")
-  end
-
-  scenario "Unauthenticated user is redirected to checkin" do
-    visit profile_path
-    
-    expect(page).to have_current_path(user_checkin_omniauth_authorize_path) 
+    expect(page.body).to have_no_selector("nav", text: "My profile")
   end
 end
 
