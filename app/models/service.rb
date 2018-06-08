469c13064afb52b44336cfd5624300437a278850
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-06-08 20:18:35 +0200

    Update main category after category is destoryed
    This operation is done using active record callbacks. If more advanced
    calculations need to be performed during category deletion this should
    be moved into service.

--- app/models/service.rb
+++ app/models/service.rb
@@ -12,20 +12,20 @@ class Service < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
 
-  after_save :set_main_category, if: :main_category_missing?
+  after_save :set_first_category_as_main!, if: :main_category_missing?
 
   def main_category
     @main_category ||= categories.joins(:service_categories).
                                   find_by(service_categories: { main: true })
   end
 
+  def set_first_category_as_main!
+    service_categories.first&.update_attributes(main: true)
+  end
+
   private
 
     def main_category_missing?
       service_categories.where(main: true).count.zero?
-    end
-
-    def set_main_category
-      service_categories.first&.update_attributes(main: true)
     end
 end
