70978505d760021ec9d9f97cedc0b9809c0502b0
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-10-22 08:41:14 +0200

    Add frozen_string_literal to all migrations

--- db/migrate/20180705112318_add_issue_id_to_project_items.rb
+++ db/migrate/20180705112318_add_issue_id_to_project_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddIssueIdToProjectItems < ActiveRecord::Migration[5.2]
   def change
     add_column :project_items, :issue_id, :integer
--- db/migrate/20180719071704_add_open_access_to_service.rb
+++ db/migrate/20180719071704_add_open_access_to_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddOpenAccessToService < ActiveRecord::Migration[5.2]
   def change
     add_column :services, :open_access, :boolean, default: false
