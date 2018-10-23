bdc319e79e5054367ae42849ae374cf7468f53d8
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-10-23 21:40:28 +0200

    Rename Offer.title to Offer.name

--- db/migrate/20181022085713_create_offers.rb
+++ db/migrate/20181022085713_create_offers.rb
@@ -1,7 +1,7 @@
 class CreateOffers < ActiveRecord::Migration[5.2]
   def change
     create_table :offers do |t|
-      t.string :title
+      t.string :name
       t.text :description
       t.integer :iid, null: false, index: true
       t.belongs_to :service, nill: false, index: true
