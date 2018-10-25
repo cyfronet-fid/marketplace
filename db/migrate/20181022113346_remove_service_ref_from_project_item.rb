b45fdf3cfb8f3ba7a9f396fc6548930dfdee7c2c
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-10-25 13:05:24 +0200

    Add missing frozen_string_literal in migrations

--- db/migrate/20181022113346_remove_service_ref_from_project_item.rb
+++ db/migrate/20181022113346_remove_service_ref_from_project_item.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveServiceRefFromProjectItem < ActiveRecord::Migration[5.2]
   def change
     remove_reference :project_items, :service, index: true, foreign: true
