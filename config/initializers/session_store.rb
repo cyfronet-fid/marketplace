6f8b83863d470e3a1edbaa01171f29c24ce7833e
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-10-29 09:34:36 +0100

    Sidekiq for background jobs
    Sidekiq is configured to process all background jobs. Currently we have
    `orders` and `mailers` queues. Status of delayed jobs can be monitored
    using administration panel (`/admin/jobs`).

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,17 +1,11 @@
 # frozen_string_literal: true
 
-if ENV["REDIS_URL"]
-  url = ENV["REDIS_URL"]
-else
-  url = "redis://localhost:6379/0/mp-session"
-end
-
 if Rails.env.test?
   Rails.application.config.session_store :cookie_store,
     key: "_mp_session"
 else
   Mp::Application.config.session_store :redis_store,
-    servers: url,
+    servers: "#{Mp::Application.config.redis_url}/mp-session",
     expire_after: 90.minutes,
     key: "_mp_session",
     threadsafe: false
