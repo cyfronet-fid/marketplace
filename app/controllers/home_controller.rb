5ebbda674b471a48921ffba191c6de409cb35c71
Jakub Sawicki <j.sawicki@cyfronet.pl>
2018-11-02 16:41:37 +0100

    Home view services categories lists

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,8 +12,8 @@ class HomeController < ApplicationController
 
     def load_categories_and_services
       @categories = Category.limit(4)
-      @categories_and_sample_services = @categories.map do |category|
-        [category, Service.joins(:service_categories).where("service_categories.category_id": category.id).order(title: :desc).limit(8).to_a]
+      @sample_services = @categories.map do |category|
+        Service.joins(:service_categories).where("service_categories.category_id": category.id).order(title: :desc).limit(8)
       end
       @other_services = Service.order(title: :desc).limit(8)
     end
