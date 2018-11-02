fdf47384da82d9dd2fbc51808962d1f65a0deafd
Paweł Gorczyca <goreck888@gmail.com>
2018-11-02 14:24:22 +0100

    [#174] Implementation of the home view
    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  include Service::Searchable
+
+  before_action :products
+
   def index
+  end
+
+  def products
+    @categories = Category.limit(4)
   end
 end
