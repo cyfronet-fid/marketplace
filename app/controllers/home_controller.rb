41168951953d525b47d44bd7d2144657aba52c06
Jakub Sawicki <j.sawicki@cyfronet.pl>
2018-11-02 16:32:14 +0100

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,18 @@
 class HomeController < ApplicationController
   include Service::Searchable
 
-  before_action :products
+  before_action :load_categories_and_services
 
   def index
   end
 
-  def products
-    @categories = Category.limit(4)
-  end
+  private
+
+    def load_categories_and_services
+      @categories = Category.limit(4)
+      @categories_and_sample_services = @categories.map do |category|
+        [category, Service.joins(:service_categories).where("service_categories.category_id": category.id).order(title: :desc).limit(8).to_a]
+      end
+      @other_services = Service.order(title: :desc).limit(8)
+    end
 end
