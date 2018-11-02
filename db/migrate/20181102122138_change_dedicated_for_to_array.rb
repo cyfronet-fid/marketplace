4ec5598283c4bf7533cbdb37a45a20d7a3f5ba07
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2018-11-02 21:36:26 +0100

    [#236] Update services seeds
    Fix dedicated_for array migration

--- db/migrate/20181102122138_change_dedicated_for_to_array.rb
+++ db/migrate/20181102122138_change_dedicated_for_to_array.rb
@@ -1,5 +1,5 @@
 class ChangeDedicatedForToArray < ActiveRecord::Migration[5.2]
   def change
-    change_column :services, :dedicated_for, :text, using: "(string_to_array(dedicated_for, ','))", array: true, null: false
+    change_column :services, :dedicated_for, :text, using: "(string_to_array(dedicated_for, ','))", array: true
   end
 end
