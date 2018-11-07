fe65132e9303a0561ba44db0340a94ffd7c44b48
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-11-07 15:48:01 +0100

    Show alert for logged in user when no active affiliation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_150915) do
+ActiveRecord::Schema.define(version: 2018_11_07_143358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -223,6 +223,7 @@ ActiveRecord::Schema.define(version: 2018_11_05_150915) do
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.integer "roles_mask"
+    t.integer "active_affiliations_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
