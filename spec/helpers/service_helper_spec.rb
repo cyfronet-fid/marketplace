53ab5dd47bec111fb08551680884fa132457c925
Andrzej Bacz <andrzej.bacz@gmail.com>
2018-11-11 23:07:09 +0100

    - word wraps in filters, fixed test for bigger icons

--- spec/helpers/service_helper_spec.rb
+++ spec/helpers/service_helper_spec.rb
@@ -4,15 +4,15 @@ require "rails_helper"
 
 RSpec.describe ServiceHelper, type: :helper do
   it "converts from decimal 4.5 value to html" do
-    expect(print_rating_stars(4.5)).to match(/<i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star-half-alt"><\/i>/)
+    expect(print_rating_stars(4.5)).to match(/<i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star-half-alt fa-lg"><\/i>/)
   end
 
   it "converts from decimal 5.0 value to html" do
-    expect(print_rating_stars(5.0)).to match(/<i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star"><\/i><i class="fas fa-star"><\/i>/)
+    expect(print_rating_stars(5.0)).to match(/<i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i><i class="fas fa-star fa-lg"><\/i>/)
   end
 
   it "converts from decimal 0.0 value to html" do
-    expect(print_rating_stars(0.0)).to match(/<i class="far fa-star"><\/i><i class="far fa-star"><\/i><i class="far fa-star"><\/i><i class="far fa-star"><\/i><i class="far fa-star"><\/i>/)
+    expect(print_rating_stars(0.0)).to match(/<i class="far fa-star fa-lg"><\/i><i class="far fa-star fa-lg"><\/i><i class="far fa-star fa-lg"><\/i><i class="far fa-star fa-lg"><\/i><i class="far fa-star fa-lg"><\/i>/)
   end
 
   it "return list of providers" do
