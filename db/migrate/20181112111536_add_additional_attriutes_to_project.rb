c7e8955dd04f802dedf9234ffd543fabf72d23ac
Marta Świątkowska <martaswiatkowska8@gmail.com>
2018-11-13 10:16:48 +0100

    [#351] Additional project attributes

---
+++ db/migrate/20181112111536_add_additional_attriutes_to_project.rb
@@ -0,0 +1,6 @@
+class AddAdditionalAttriutesToProject < ActiveRecord::Migration[5.2]
+  def change
+    add_column :projects, :reason_for_access, :text
+    add_column :projects, :customer_typology, :string
+  end
+end
