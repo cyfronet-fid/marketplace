eab224cb4d50688f9c9f2ae4bed0d68888a1f4f8
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-11-16 15:00:16 +0100

    Remove user id from form and use current_user

--- app/mailers/service_mailer.rb
+++ app/mailers/service_mailer.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class ServiceMailer < ApplicationMailer
-  def new_question(recipient_email, service_question, service)
+  def new_question(recipient_email, author, service_question, service)
     @service = service
     @message = service_question[:text]
-    @author = User.find(service_question[:author])
+    @author = author
 
     mail(to: recipient_email,
          from: @author.email,
