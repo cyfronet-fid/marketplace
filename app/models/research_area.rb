1fb692409afa0fa85a26aba11cb8420a0be2e7da
Marek Kasztelnik <mkasztelnik@gmail.com>
2018-11-19 14:39:40 +0100

    Research area hierarchy

--- app/models/research_area.rb
+++ app/models/research_area.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ResearchArea < ApplicationRecord
+  has_ancestry cache_depth: true
+
   has_many :service_research_areas, autosave: true, dependent: :destroy
   has_many :services, through: :service_research_areas
 
---
+++ db/migrate/20181119100216_add_ancestry_to_research_area.rb
@@ -0,0 +1,6 @@
+class AddAncestryToResearchArea < ActiveRecord::Migration[5.2]
+  def change
+    add_column :research_areas, :ancestry, :string, index: true
+    add_column :research_areas, :ancestry_depth, :integer, default: 0
+  end
+end
