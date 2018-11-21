2a1f9ccc01e831f0e90d13fb006ec1f0cf47eb63
Michał Szostak <michal.szostak@cyfronet.pl>
2018-11-21 13:38:41 +0100

    [#232] Match MP order states with JIRA
    * Add workflow step `rejected`
    * Add workflow step `waiting_for_response`
    
    Fixes: #232

--- app/models/jira/client.rb
+++ app/models/jira/client.rb
@@ -5,7 +5,7 @@ class Jira::Client < JIRA::Client
   attr_reader :jira_project_key
   attr_reader :jira_issue_type_id
   attr_reader :webhook_secret
-  attr_reader :wf_todo_id, :wf_in_progress_id, :wf_done_id
+  attr_reader :wf_todo_id, :wf_in_progress_id, :wf_done_id, :wf_rejected_id, :wf_waiting_for_response_id
 
   def initialize
     # read required options and initialize jira client
@@ -27,6 +27,8 @@ class Jira::Client < JIRA::Client
     @wf_todo_id = @jira_config["workflow"]["todo"]
     @wf_in_progress_id = @jira_config["workflow"]["in_progress"]
     @wf_done_id = @jira_config["workflow"]["done"]
+    @wf_rejected_id = @jira_config["workflow"]["rejected"]
+    @wf_waiting_for_response_id = @jira_config["workflow"]["waiting_for_response"]
 
     super(options)
   end
