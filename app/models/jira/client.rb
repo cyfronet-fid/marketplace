ccae80ccb3760b1972d5cbc947c4d1c58d1b555c
Michał Szostak <michal.szostak@cyfronet.pl>
2018-11-22 15:22:21 +0100

    [#466] Mapping order (project item) data to JIRA issue
    * Add custom fields mapping for JIRA
    * Add custom field checks and suggestions to checked (and
      `rake jira:check` task)
    
    Fixes: #466

--- app/models/jira/client.rb
+++ app/models/jira/client.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 class Jira::Client < JIRA::Client
+  include Rails.application.routes.url_helpers
+
   attr_reader :jira_config
   attr_reader :jira_project_key
   attr_reader :jira_issue_type_id
   attr_reader :webhook_secret
+  attr_reader :custom_fields
   attr_reader :wf_todo_id, :wf_in_progress_id, :wf_done_id, :wf_rejected_id, :wf_waiting_for_response_id
 
   def initialize
@@ -30,7 +33,46 @@ class Jira::Client < JIRA::Client
     @wf_rejected_id = @jira_config["workflow"]["rejected"]
     @wf_waiting_for_response_id = @jira_config["workflow"]["waiting_for_response"]
 
+    fields_config = @jira_config["custom_fields"]
+
+    @custom_fields = {
+      "Order reference": fields_config["Order reference"],
+      "CI-Name": fields_config["CI-Name"],
+      "CI-Surname": fields_config["CI-Surname"],
+      "CI-Email": fields_config["CI-Email"],
+      "CI-DisplayName": fields_config["CI-DisplayName"],
+      "CI-EOSC-UniqueID": fields_config["CI-EOSC-UniqueID"],
+      "CI-Institution": fields_config["CI-Institution"],
+      "CI-Department": fields_config["CI-Department"],
+      "CI-DepartamentalWebPage": fields_config["CI-DepartamentalWebPage"],
+      "CI-SupervisorName": fields_config["CI-SupervisorName"],
+      "CI-SupervisorProfile": fields_config["CI-SupervisorProfile"],
+      "CP-CustomerTypology": fields_config["CP-CustomerTypology"],
+      "CP-ReasonForAccess": fields_config["CP-ReasonForAccess"],
+      # "CP-UserGroupName": "?", TODO -
+      # "CP-ProjectInformation": "?", TODO -
+      # For now only single Service Offer is supported
+      "SO-1": fields_config["SO-1"]
+    }
+
     super(options)
+  end
+
+  def create_service_issue(project_item)
+    issue = self.Issue.build
+
+    fields = { summary: "Service order, #{project_item.project.user.first_name} " +
+                       "#{project_item.project.user.last_name}, " +
+                       "#{project_item.service.title}",
+              project: { key: @jira_project_key },
+              issuetype: { id: @jira_issue_type_id },
+    }
+
+    @custom_fields.reject { |k, v| v.empty? }.each do |field_name, field_id|
+      fields[field_id.to_s] = generate_custom_field_value(field_name.to_s, project_item)
+    end
+
+    issue.save(fields: fields) ? issue : nil
   end
 
   def mp_issue_type
@@ -39,5 +81,48 @@ class Jira::Client < JIRA::Client
 
   def mp_project
     self.Project.find(@jira_project_key)
+  end
+
+private
+
+  def encode_properties(properties)
+    properties.map { |p| "#{p["label"]}=#{p["value"]}" }.join("&")
+  end
+
+  def generate_custom_field_value(field_name, project_item)
+    case field_name
+    when "Order reference"
+      ENV["ROOT_URL"].blank? ? project_item_path(project_item) : project_item_url(project_item, host: ENV["ROOT_URL"])
+    when "CI-Name"
+      project_item.project.user.first_name
+    when "CI-Surname"
+      project_item.project.user.last_name
+    when "CI-Email"
+      project_item.affiliation&.email || ""
+    when "CI-DisplayName"
+      "#{project_item.project.user.first_name} #{project_item.project.user.last_name}"
+    when "CI-EOSC-UniqueID"
+      project_item.project.user.uid
+    when "CI-Institution"
+      project_item.affiliation&.organization || ""
+    when "CI-Department"
+      project_item.affiliation&.department || ""
+    when "CI-DepartamentalWebPage"
+      project_item.affiliation&.webpage || ""
+    when "CI-SupervisorName"
+      project_item.affiliation&.supervisor || ""
+    when "CI-SupervisorProfile"
+      project_item.affiliation&.supervisor_profile || ""
+    when "CP-CustomerTypology"
+      project_item.customer_typology
+    when "CP-ReasonForAccess"
+      project_item.access_reason
+    when "SO-1"
+      "#{project_item.service.categories.first.name}/" +
+      "#{project_item.service.title}/" +
+      "#{encode_properties(project_item.properties)}"
+    else
+      ""
+    end
   end
 end
--- spec/models/jira/client_spec.rb
+++ spec/models/jira/client_spec.rb
@@ -16,4 +16,76 @@ describe Jira::Client do
     expect(client).to receive_message_chain("Project.find").with(client.jira_project_key).and_return(return_val)
     expect(client.mp_project).to equal(return_val)
   end
+
+  it "create_service issue should save issue with correct fields" do
+    allow(ENV).to receive(:[]).and_call_original
+    allow(ENV).to receive(:[]).with("ROOT_URL").and_return("https://mp.edu")
+
+    user = create(:user, first_name: "John", last_name: "Doe", uid: "uid1")
+    project_item = create(:project_item,
+                          affiliation: create(:affiliation,
+                                              user: user,
+                                              organization: "organization 1",
+                                              department: "department 1",
+                                              email: "john.doe@organization.com", webpage: "http://organization.com",
+                                              supervisor: "Jim Supervisor",
+                                              supervisor_profile: "http://jim.supervisor.edu"),
+                          offer: create(:offer, service: create(:service,
+                                                                title: "s1",
+                                                                categories: [create(:category, name: "cat1")])),
+                          project: create(:project, user: user),
+                          customer_typology: "single_user",
+                          access_reason: "some reason", properties: [
+            {
+                "id": "id1",
+                "type": "input",
+                "label": "Data repository name",
+                "value": "aaaaaa",
+                "value_type": "string",
+                "description": "Type data repository name" },
+            {
+                "id": "id2",
+                "type": "select",
+                "label": "Harvesting method",
+                "value": "OAI-PMH",
+                "config": { "mode": "buttons", "values": ["OAI-PMH", "JSON-API", "CSW 2.0", "Other"] },
+                "value_type": "string",
+                "description": "Choose harvesting method"
+            },
+            {
+                "id": "id3",
+                "type": "input",
+                "label": "Harvesting endpoint",
+                "value": "aaaaa",
+                "value_type": "string",
+                "description": "Type harvesting endpoint" }
+        ])
+
+    expected_fields = { summary: "Service order, John Doe, s1",
+                       project: { key: "MP" },
+                       issuetype: { id: 10000 },
+                       "Order reference-1" => Rails.application.routes.url_helpers.project_item_url(id: project_item.id,
+                                                                                                    host: "https://mp.edu"),
+                       "CI-Name-1" => "John",
+                       "CI-Surname-1" => "Doe",
+                       "CI-Email-1" => "john.doe@organization.com",
+                       "CI-DisplayName-1" => "John Doe",
+                       "CI-EOSC-UniqueID-1" => "uid1",
+                       "CI-Institution-1" => "organization 1",
+                       "CI-Department-1" => "department 1",
+                       "CI-DepartamentalWebPage-1" => "http://organization.com",
+                       "CI-SupervisorName-1" => "Jim Supervisor",
+                       "CI-SupervisorProfile-1" => "http://jim.supervisor.edu",
+                       "CP-CustomerTypology-1" => "single_user",
+                       "CP-ReasonForAccess-1" => "some reason",
+                       "SO-1-1" => "cat1/s1/Data repository name=aaaaaa&" +
+                                   "Harvesting method=OAI-PMH&" +
+                                   "Harvesting endpoint=aaaaa" }
+
+    issue = double(:Issue)
+    expect(issue).to receive("save").with(fields: expected_fields).and_return(true)
+    expect(client).to receive_message_chain("Issue.build").and_return(issue)
+
+    expect(client.create_service_issue(project_item)).to be(issue)
+  end
 end
