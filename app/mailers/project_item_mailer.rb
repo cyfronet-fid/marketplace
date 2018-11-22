7994201b8c7919e95647620d1bb4d3298bd2e9ef
Roksana Rozanska <r.rozanska@cyfronet.pl>
2018-11-22 20:00:38 +0100

    last changes
    improved text shown to user after order is created
    Improved subjects of project item emails
    added links in clarin services

--- app/mailers/project_item_mailer.rb
+++ app/mailers/project_item_mailer.rb
@@ -5,7 +5,7 @@ class ProjectItemMailer < ApplicationMailer
     @project_item = project_item
     @user = project_item.user
 
-    mail(to: @user.email, subject: "#{prefix(project_item)} created")
+    mail(to: @user.email, subject: "New request for service access in EOSC Portal Marketplace created")
   end
 
   def changed(project_item)
@@ -34,7 +34,7 @@ class ProjectItemMailer < ApplicationMailer
 
   def new_message(project_item)
     mail(to: @user,
-         subject: "#{prefix(project_item)} new message",
+         subject: "Question about your service access request in EOSC Portal Marketplace",
          template_name: "new_message")
   end
 
@@ -42,7 +42,7 @@ class ProjectItemMailer < ApplicationMailer
 
     def status_changed(project_item)
       mail(to: @user,
-            subject: "#{prefix(project_item)} status changed",
+            subject: "Status of your service access request in EOSC Portal Marketplace has changed",
             template_name: "changed")
     end
 
