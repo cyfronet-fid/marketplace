988278520f912bddbfed9190ece53d694d95ae8f
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-01-23 08:31:42 +0100

    `back_link_to` which deals with record state
    This helper method will generate link to the record if it is persisted,
    otherwise link to index page is generated.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 describe ApplicationHelper, type: :helper do
-  describe "current_controller?" do
+  context "#current_controller?" do
     before do
       allow(controller).to receive(:controller_name).and_return("foo")
     end
@@ -22,7 +22,7 @@ describe ApplicationHelper, type: :helper do
     end
   end
 
-  describe "current_action?" do
+  context "#current_action?" do
     before do
       allow(controller).to receive(:action_name).and_return("foo")
     end
@@ -38,6 +38,32 @@ describe ApplicationHelper, type: :helper do
     it "should take any number of arguments" do
       expect(current_action?(:baz, :bar)).to be_falsy
       expect(current_action?(:baz, :bar, :foo)).to be_truthy
+    end
+  end
+
+  context "#back_link_to" do
+    it "returns to index for not persisted object" do
+      expect(back_link_to("Back", build(:service))).
+        to eq("<a href=\"#{services_path}\">Back</a>")
+    end
+
+    it "returns to show for persisted object" do
+      service = create(:service)
+
+      expect(back_link_to("Back", service)).
+        to eq("<a href=\"#{service_path(service)}\">Back</a>")
+    end
+
+    it "respects prefix" do
+      service = create(:service)
+
+      expect(back_link_to("Back", service, prefix: :backoffice)).
+        to eq("<a href=\"#{backoffice_service_path(service)}\">Back</a>")
+    end
+
+    it "passes html options" do
+      expect(back_link_to("Back", build(:service), class: "btn")).
+        to eq("<a class=\"btn\" href=\"#{services_path}\">Back</a>")
     end
   end
 end
