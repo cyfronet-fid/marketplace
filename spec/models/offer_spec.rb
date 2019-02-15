0b74e3a68dec3a0ea9c4e4878dc1490e42b17f3d
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-02-15 14:32:03 +0100

    Offer type with service type as default
    Offer can be customized and dedicated type can be set there. If type is
    not set default is taken from `Service#service_type`.

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Offer do
   it { should belong_to(:service) }
 
   it { should have_many(:project_items).dependent(:restrict_with_error) }
+
+  context "#offer_type" do
+    it "takes default from service if not set" do
+      service = create(:service, service_type: :catalog)
+      offer = create(:offer, service: service)
+
+      expect(offer.offer_type).to eq "catalog"
+    end
+  end
 end
