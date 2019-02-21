97fa59d2e4e59c9aca27ecac42ca819f4b52129b
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-02-21 09:53:19 +0100

    Date attribute min and max validation

---
+++ app/helpers/formats_helper.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module FormatsHelper
+  def to_js_date_format(date_format)
+    date_format
+      .gsub("%Y", "yyyy")
+      .gsub("%m", "mm")
+      .gsub("%d", "dd")
+  end
+end
---
+++ spec/models/attribute/date_spec.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Attribute::Date do
+  context "min validation" do
+    let(:date_attribute) { Attribute.from_json(attr_json_with_config("min" => "now")) }
+
+    it "is ok for current day" do
+      date_attribute.value = I18n.l(Date.today)
+
+      expect(date_attribute).to be_valid
+    end
+
+    it "is ok for tomorrow" do
+      date_attribute.value = I18n.l(Date.today + 1.day)
+
+      expect(date_attribute).to be_valid
+    end
+
+    it "is invalid for yesterday" do
+      date_attribute.value = I18n.l(Date.today - 1.day)
+
+      expect(date_attribute).to_not be_valid
+      expect(date_attribute.errors[:id][0]).to start_with("Please select date after")
+    end
+  end
+
+  context "min validation" do
+    let(:date_attribute) { Attribute.from_json(attr_json_with_config("max" => "now")) }
+
+    it "is ok for current day" do
+      date_attribute.value = I18n.l(Date.today)
+
+      expect(date_attribute).to be_valid
+    end
+
+    it "is ok for yesterday" do
+      date_attribute.value = I18n.l(Date.today - 1.day)
+
+      expect(date_attribute).to be_valid
+    end
+
+    it "is invalid for tomorrow" do
+      date_attribute.value = I18n.l(Date.today + 1.day)
+
+      expect(date_attribute).to_not be_valid
+      expect(date_attribute.errors[:id][0]).to start_with("Please select date before")
+    end
+  end
+
+  def attr_json_with_config(config)
+    { "id" => "id6",
+      "label" => "Start of service",
+      "description" => "Please choose start date",
+      "type" => "date",
+      "value_type" => "string",
+      "config" => config }
+  end
+end
