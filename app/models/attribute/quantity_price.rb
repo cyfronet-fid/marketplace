1bb16904e9ed6ec2255c7b0de4494dc3a07c0726
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-02-22 14:41:32 +0100

    Quantity price attriubute
    Example configuration:
    
    ```
    {
      "id"=>"id7",
      "type"=>"quantity_price",
      "label"=>"Price per unit",
      "config"=>{"max"=>15, "currency"=>"EUR", "step_price"=>100, "start_price"=>1024},
      "value_type"=>"integer",
      "description"=>"Choose quantity"
    }
    ```

---
+++ app/models/attribute/quantity_price.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+require "json-schema"
+
+class Attribute::QuantityPrice < Attribute
+  def value_type_schema
+    {
+        "type": "string",
+        "enum": ["integer"]
+    }
+  end
+
+  def config_schema
+    {
+      "type": "object",
+      "required": ["start_price", "step_price", "currency"],
+      "properties": {
+        "start_price": { "type": "integer", "minimum": 0 },
+        "step_price":  { "type": "integer", "minimum": 0 },
+        "currency":    { "type": "string" },
+        "max":         { "type": "integer", "minimum": 0 }
+      }
+    }
+  end
+
+  def value_validity
+    super
+
+    errors.add(:id, "Quantity need to be greater or equal to 0") if to_small?
+    errors.add(:id, "Quantity need to be lower or equal to #{max}") if to_big?
+  end
+
+
+  def value_type
+    "integer"
+  end
+
+  def value_valid?
+    Integer(@value) rescue false
+  end
+
+  def start_price
+    config["start_price"]
+  end
+
+  def step_price
+    config["step_price"]
+  end
+
+  def max
+    config["max"]
+  end
+
+  def currency
+    config["currency"]
+  end
+
+  protected
+
+    TYPE = "quantity_price"
+
+  private
+
+    def to_small?
+      value && value < 0
+    end
+
+    def to_big?
+      value && max && value > max
+    end
+end
