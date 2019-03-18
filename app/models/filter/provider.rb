a7f8ee81842e8c79582ed702f0bfe179e4338826
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-03-18 10:58:13 +0100

    Externalize duplicated code to multiselect filter

--- app/models/filter/provider.rb
+++ app/models/filter/provider.rb
@@ -1,29 +1,15 @@
 # frozen_string_literal: true
 
-class Filter::Provider < Filter
+class Filter::Provider < Filter::Multiselect
   def initialize(params = {})
     super(params: params.fetch(:params, {}),
-          field_name: "providers", type: :multiselect,
-          title: "Providers")
-
-    @category = params[:category]
+          category: params[:category],
+          field_name: "providers",
+          title: "Providers",
+          query: ::Provider.select("providers.name, providers.id, count(service_providers.service_id) as service_count"))
   end
 
   private
-
-    def fetch_options
-      query = ::Provider.select("providers.name, providers.id, count(service_providers.service_id) as service_count")
-
-      if @category.nil?
-        query = query.joins(:services)
-      else
-        query = query.joins(:categories).where("categories.id = ?", @category.id)
-      end
-
-      query.group("providers.id")
-          .order(:name)
-          .map { |provider| [provider.name, provider.id, provider.service_count] }
-    end
 
     def do_call(services)
       services.joins(:service_providers).group("services.id")
--- app/models/filter/target_group.rb
+++ app/models/filter/target_group.rb
@@ -1,30 +1,15 @@
 # frozen_string_literal: true
 
-class Filter::TargetGroup < Filter
+class Filter::TargetGroup < Filter::Multiselect
   def initialize(params = {})
     super(params: params.fetch(:params, {}),
-          field_name: "target_groups", type: :multiselect,
-          title: "For")
-    @category = params[:category]
+          category: params[:category],
+          field_name: "target_groups",
+          title: "For",
+          query: ::TargetGroup.select("target_groups.name, target_groups.id, count(services.id) as service_count"))
   end
 
   private
-
-    def fetch_options
-      @options_target_groups ||= begin
-        query = ::TargetGroup.select("target_groups.name, target_groups.id, count(services.id) as service_count")
-
-        if @category.nil?
-          query = query.joins(:services)
-        else
-          query = query.joins(:categories).where("categories.id = ?", @category.id)
-        end
-
-        query.group("target_groups.id")
-            .order(:name)
-            .map { |target_group| [target_group.name, target_group.id, target_group.service_count] }
-      end
-    end
 
     def do_call(services)
       services.joins(:service_target_groups).
