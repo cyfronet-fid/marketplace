02014ce3c20d9a6f771432b011be8d3325ae110d
Jaroslaw Zbozien <j.zbozien@gmail.com>
2019-03-26 16:54:38 +0100

    custom mods

--- app/helpers/multicheckbox_helper.rb
+++ app/helpers/multicheckbox_helper.rb
@@ -7,13 +7,20 @@ module MulticheckboxHelper
     end
 
     result = "<div class=\"collapse\" data-controller=\"multicheckbox\" id=\"#{name}\">"
+
+    result += "<div data-target=\"multicheckbox.element\" class=\"checkbox empty\"><label class=\"small\">
+                 <input class=\"form-check-input\" name=\"providers[]\" multiple=\"true\" type=\"checkbox\" value=\"9\">
+                 <span class=\"cr\"><i class=\"cr-icon fas fa-check\"></i></span>sample empty filter</label>
+                 <span class=\"float-right counter\">0</span></div>"
+
     options.each do | option |
       disabled = (option[2] == 0)
-      result += "<div data-target=\"multicheckbox.element\"><label
+      result += "<div data-target=\"multicheckbox.element\" class=\"checkbox\"><label
                  data-multicheckbox class=\"#{disabled ? "text-muted" : ""} small\">
                  <input class=\"form-check-input\" #{disabled ? "disabled=\"disabled\"" : ""} name=\"#{name}[]\"
-                        multiple=\"true\" type=\"checkbox\" #{value.include?(option[1].to_s) ? "checked=\"checked\"" : ""} value=\"#{option[1]}\">#{option[0]}</label>
-                 <span class=\"float-right small #{disabled ? "text-muted" : ""}\">#{option[2]}</span></div>"
+                        multiple=\"true\" type=\"checkbox\" #{value.include?(option[1].to_s) ? "checked=\"checked\"" : ""} value=\"#{option[1]}\">
+                 <span class=\"cr\"><i class=\"cr-icon fas fa-check\"></i></span>#{option[0]}</label>
+                 <span class=\"float-right counter #{disabled ? "text-muted" : ""}\">#{option[2]}</span></div>"
     end
 
     result += "<a data-target=\"multicheckbox.toggler\" href=\"javascript:undefined\"
