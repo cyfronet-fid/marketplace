8388712ea71ad3898865d3ef64712d4b526f0c5e
Michał Szostak <michal.szostak@cyfronet.pl>
2019-03-26 22:52:27 +0100

    [#779] Provider external ID
    * Add DB table storing provider's external IDs
    * Add form controls to provider's backoffice form allowing setting of
      external sources
    * Update backoffice provider form's buttons styling to match services
    * Add information about external sources to backoffice provider's detail
      view
    
    Closes: #779

---
+++ db/migrate/20190326203609_create_provider_sources_table.rb
@@ -0,0 +1,11 @@
+class CreateProviderSourcesTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :provider_sources do |t|
+      t.string :eid, null: false
+      t.string :source_type, null: false
+      t.belongs_to :provider, index: true, null: false
+      t.timestamps
+    end
+    add_index :provider_sources, [:eid, :source_type, :provider_id], unique: true
+  end
+end
