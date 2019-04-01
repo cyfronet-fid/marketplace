094a8bc7173e665716bc818786aa4f7b567647d2
Andrzej Bacz <andrzej.bacz@gmail.com>
2019-04-01 14:18:41 +0200

    Create branch commit Branch for static element development

--- app/helpers/multicheckbox_helper.rb
+++ app/helpers/multicheckbox_helper.rb
@@ -8,11 +8,6 @@ module MulticheckboxHelper
 
     result = "<div class=\"collapse\" data-controller=\"multicheckbox\" id=\"#{name}\">"
 
-    result += "<div data-target=\"multicheckbox.element\" class=\"checkbox empty\"><label class=\"small\">
-                 <input class=\"form-check-input\" name=\"providers[]\" multiple=\"true\" type=\"checkbox\" value=\"9\">
-                 <span class=\"cr\"><i class=\"cr-icon fas fa-check\"></i></span>sample empty filter</label>
-                 <span class=\"float-right counter\">0</span></div>"
-
     options.each do | option |
       disabled = (option[2] == 0)
       result += "<div data-target=\"multicheckbox.element\" class=\"checkbox\"><label
