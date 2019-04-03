baa79a4e8e0b47e08101c719edeac401e6b1600f
Marta Świątkowska <martaswiatkowska8@gmail.com>
2019-04-03 14:02:02 +0200

    Elasticsearch autocomplete

---
+++ app/controllers/concerns/service/autocomplete.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Service::Autocomplete
+  extend ActiveSupport::Concern
+
+  def autocomplete
+    query = Service.search(params[:q],
+      fields: ["title"],
+      operator: "or",
+      match: :word_middle,
+      limit: 5,
+      load: false,
+      highlight: { tag: "<b>" }
+    )
+    html_results = query.highlights.map.with_index { |s, i| "<li class=\"dropdown-item\" role=\"option\" data-autocomplete-value=\"#{i}\">#{s[:title]}</li>" }.join
+
+    respond_to do |format|
+      format.html do
+        render status: :ok, html: html_results.html_safe
+      end
+    end
+  end
+end
