d6d1901bc9df06771531fd7cca14c0fd3fe6a419
Bartosz Wilk <b.wilk@cyfronet.pl>
2019-05-06 15:34:42 +0200

    Using Elasticsearch aggergations for fetching filter options

--- app/controllers/concerns/service/searchable.rb
+++ app/controllers/concerns/service/searchable.rb
@@ -9,7 +9,11 @@ module Service::Searchable
   end
 
   def search(search_scope)
-    query_present? ? search_fields(search_scope) : paginate(order(search_scope.distinct))
+    query_present? ? (Service.search *(complex_params(search_scope))) : paginate(order(search_scope.distinct))
+  end
+
+  def search_simple(search_scope)
+    query_present? ? (Service.search *(simple_params(search_scope))) : search_scope.distinct
   end
 
   private
@@ -18,16 +22,29 @@ module Service::Searchable
       params[:q].present?
     end
 
-    def search_fields(search_scope)
-      Service.search params[:q],
-                     fields: [ "title^7", "tagline^3", "description"],
-                     operator: "or",
-                     where: { id: search_scope.ids },
-                     page: params[:page], per_page: per_page,
-                     order: params[:sort].blank? ? nil : ordering,
-                     match: :word_middle,
-                     highlight: { fields: [:title, :tagline], tag: "<b>" },
-                     scope_results: ->(r) { r.includes(:research_areas, :providers, :target_groups).with_attached_logo }
+    def simple_params(search_scope)
+      [
+          params[:q],
+          fields: [ "title^7", "tagline^3", "description"],
+          operator: "or",
+          where: { id: search_scope.ids },
+          match: :word_middle,
+      ]
+    end
+
+    def complex_params(search_scope)
+      [
+          params[:q],
+          fields: [ "title^7", "tagline^3", "description"],
+          operator: "or",
+          where: { id: search_scope.ids },
+          page: params[:page],
+          per_page: per_page,
+          order: params[:sort].blank? ? nil : ordering,
+          match: :word_middle,
+          highlight: { fields: [:title], tag: "<b>" },
+          scope_results: ->(r) { r.includes(:research_areas, :providers, :target_groups).with_attached_logo }
+      ]
     end
 
     def highlights(from_search)
--- app/models/service.rb
+++ app/models/service.rb
@@ -15,7 +15,12 @@ class Service < ApplicationRecord
       tagline: tagline,
       description: description,
       status: status,
-      rating: rating
+      rating: rating,
+      categories: categories.map(&:id),
+      research_areas: research_areas.map(&:id),
+      providers: providers.map(&:id),
+      platforms: platforms.map(&:id),
+      target_groups: service_target_groups.map(&:id),
     }
   end
 
