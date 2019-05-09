6e01302f7924c140f340176b2340eafb7a6d1821
Marta Świątkowska <martaswiatkowska8@gmail.com>
2019-05-09 14:58:01 +0200

    Elasticsearch index changes
    - Added tagline to elasticsearch index
    - setted different weights for elasticsearch fields
    - added highlights for tagline

--- app/controllers/concerns/service/searchable.rb
+++ app/controllers/concerns/service/searchable.rb
@@ -20,13 +20,13 @@ module Service::Searchable
 
     def search_fields(search_scope)
       Service.search params[:q],
-                     fields: [ "title^5", "description"],
+                     fields: [ "title^7", "tagline^3", "description"],
                      operator: "or",
                      where: { id: search_scope.ids },
                      page: params[:page], per_page: per_page,
                      order: params[:sort].blank? ? nil : ordering,
                      match: :word_middle,
-                     highlight: { fields: [:title], tag: "<b>" },
+                     highlight: { fields: [:title, :tagline], tag: "<b>" },
                      scope_results: ->(r) { r.includes(:research_areas, :providers, :target_groups).with_attached_logo }
     end
 
