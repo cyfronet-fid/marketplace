fcf162d95292b14cff07071697f5c086a7bc6937
Bartosz Wilk <b.wilk@cyfronet.pl>
2019-06-07 20:17:04 +0200

    Fixed ancestry multiselect name method

--- spec/models/filter/research_area_spec.rb
+++ spec/models/filter/research_area_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Filter::ResearchArea do
       create(:service, research_areas: [child1, child2])
       create(:service, research_areas: [child2])
 
-      counters = { root.id => 3, child1.id => 1, child2.id => 2 }
+      counters = { root.id => 1, child1.id => 1, child2.id => 2 }
       subject.counters = counters
 
       options = subject.options
@@ -22,13 +22,30 @@ RSpec.describe Filter::ResearchArea do
       first_option = options.first
       expect(first_option[:name]).to eq(root.name)
       expect(first_option[:id]).to eq(root.id)
-      expect(first_option[:count]).to eq(3)
+      expect(first_option[:count]).to eq(1)
 
       expect(first_option[:children]).to contain_exactly(
         { name: child1.name, id: child1.id, count: 1, children: [] },
         { name: child2.name, id: child2.id, count: 2, children: [] })
     end
   end
+
+  context "#active filters" do
+    it "returns correct active filters" do
+      parent = create(:research_area)
+      child1, child2 = create_list(:research_area, 2, parent: parent)
+      create(:service, research_areas: [child2])
+
+      params = ActionController::Parameters.new("research_areas" => [child1.id.to_s, parent.id.to_s])
+
+      filter = described_class.new(params: params)
+      filter.counters = { child2.id => 1 }
+
+      expect(filter.active_filters).
+          to include([parent.name, "research_areas" => [child1.id.to_s]], [child1.name, "research_areas" => [parent.id.to_s]])
+    end
+  end
+
   context "#constraint" do
     it "use parent and all children when parent is selected" do
       parent = create(:research_area)
