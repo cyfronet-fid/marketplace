e045271ac44860887d7b9b657f5db575ec9a2ada
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-06-19 12:52:52 +0200

    Remove affiliations from the views

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,5 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @affiliations = policy_scope(Affiliation).order(:iid).page(params[:page])
   end
 end
