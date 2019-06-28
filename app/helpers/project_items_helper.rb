fe49b6d2f8360d059ed0007d44a86b067f54c7ad
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-06-28 15:46:33 +0200

    Project card redesigned

--- app/helpers/project_items_helper.rb
+++ app/helpers/project_items_helper.rb
@@ -22,4 +22,12 @@ module ProjectItemsHelper
   def ratingable?
     (@project_item.ready? && @project_item.service_opinion.nil?)
   end
+
+  def project_item_status(project_item)
+    if project_item.in_progress?
+      content_tag(:i, nil, class: "fas fa-spinner")
+    else
+      content_tag(:i, nil, class: "fas fa-circle status-#{project_item.status.dasherize}")
+    end
+  end
 end
