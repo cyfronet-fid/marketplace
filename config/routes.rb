3b4fcd9408b4a7675054457e6aeb8740756e98c3
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-07-12 12:13:17 +0200

    Project view have projects navigation

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   resources :projects do
     scope module: :projects do
       resources :about, only: :index
+      resources :services, only: [:show, :index]
       resource :chat, only: :show
     end
   end
