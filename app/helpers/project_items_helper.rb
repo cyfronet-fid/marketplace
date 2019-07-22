c66b1c6f7e407175a0bbd9e853372a76b148f016
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-07-22 11:45:57 +0200

    Split project item into details and conversation
    Similar structure like for project is used.

--- app/helpers/project_items_helper.rb
+++ app/helpers/project_items_helper.rb
@@ -24,4 +24,10 @@ module ProjectItemsHelper
       content_tag(:i, nil, class: "fas fa-circle status-#{project_item.status}")
     end
   end
+
+  def service_providers_list(project_item)
+    providers = project_item.service.providers.
+      map { |p| link_to(p.name, services_path(providers: p)) }
+    safe_join(providers, ", ")
+  end
 end
