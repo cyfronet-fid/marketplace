6a6cbafd4b59e273e69dd36d703061c903388f03
Bartosz Wilk <b.wilk@cyfronet.pl>
2019-08-13 11:37:26 +0200

    Service Carousel test

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -14,4 +14,14 @@ RSpec.feature "Home" do
     expect(page).to have_current_path(services_path, ignore_query: true)
     expect(page).to have_selector("#q[value='Something']")
   end
+
+  context "services carousel" do
+    let!(:service1) { create(:service, title: "published-service-123", status: :published) }
+    let!(:service2) { create(:service, title: "draft-service-123", status: :draft) }
+    it "should not show not published service at carousel" do
+      visit "/"
+      expect(page).to have_selector("div.card-title", text: "published-service-123")
+      expect(page).not_to have_selector("div.card-title", text: "draft-service-123")
+    end
+  end
 end
