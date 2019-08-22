2f94096bf4c2be05d7b049b9c689b8e1798c27d4
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2019-08-22 18:44:47 +0200

    Add loading items from database
    Items loaded on the start page:
    - providers
    - platforms
    - target_groups
    - branch_of_science
    - categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :load_services
+  before_action :load_services, :load_platforms, :load_providers, :load_target_groups
 
   def index
   end
@@ -13,5 +13,20 @@ class HomeController < ApplicationController
       @services_number = Service.count
       @countries_number = 32
       @services = Service.published.includes(:providers).order(rating: :asc, title: :desc).limit(8)
+    end
+
+    def load_platforms
+      @home_platforms = Platform.joins(:services).uniq.sample(10).map
+      @home_platforms_counter = Platform.all.count - @home_platforms.count
+    end
+
+    def load_providers
+      @home_providers = Provider.joins(:services).uniq.sample(5)
+      @home_providers_counter = Provider.all.count - @home_providers.count
+    end
+
+    def load_target_groups
+      @home_target_groups = TargetGroup.all.first(5)
+      @home_target_groups_counter = TargetGroup.all.count - @home_target_groups.count
     end
 end
