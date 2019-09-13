db95bd6018528be5aee56b9d1ae4855ccdaacaab
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2019-09-13 17:29:25 +0200

    [WIP] Service preview page

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
   resource :backoffice, only: :show
   namespace :backoffice do
     resources :services do
-      get "preview", via: :post
+      post "preview", on: :member
       scope module: :services do
         resources :offers
         resources :offers do
