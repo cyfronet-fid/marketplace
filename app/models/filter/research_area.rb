da635d1bc38f943d429096a61be47126848749c9
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-09-27 09:58:34 +0200

    Multi checkbox filters search

--- app/models/filter/research_area.rb
+++ app/models/filter/research_area.rb
@@ -4,6 +4,7 @@ class Filter::ResearchArea < Filter::AncestryMultiselect
   def initialize(params = {})
     super(params: params.fetch(:params, {}),
           field_name: "research_areas", title: "Research Area",
-          model: ::ResearchArea, joining_model: ServiceResearchArea, index: "research_areas")
+          model: ::ResearchArea, joining_model: ServiceResearchArea,
+          index: "research_areas", search: true)
   end
 end
