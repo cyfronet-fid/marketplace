c17f08bb00d6112fcdf79b3860dc8ac67b228a6c
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-10-01 15:11:12 +0200

    Filter specs refactored

--- spec/features/filter_spec.rb
+++ spec/features/filter_spec.rb
@@ -2,11 +2,69 @@
 
 require "rails_helper"
 
-RSpec.feature "Service filtering" do
+RSpec.feature "Service filter" do
   include OmniauthHelper
 
-  context "research area" do
-    it "is hierarchical" do
+  context "wrapper" do
+    it "can be closed", js: true do
+      create(:provider, name: "Cyfronet provider")
+
+      # By default all filters are expanded
+      visit services_path
+      expect(page).to have_text("Cyfronet provider")
+      click_on "Providers"
+      expect(page).to_not have_text("Cyfronet provider")
+    end
+
+    it "stores collapsable state", js: true do
+      create(:research_area, name: "Science!")
+
+      visit services_path
+      expect(page).to have_text("Science!")
+      click_on "Research Area"
+      visit services_path
+      expect(page).to_not have_text("Science!")
+    end
+  end
+
+  it "respects seach query", js: true do
+    visit services_path(q: "Funcy search phrase")
+    select "EU"
+
+    expect(body).to have_text("Funcy search phrase")
+  end
+
+  it "reset page after filtering", js: true do
+    create_list(:service, 5)
+
+    visit services_path(page: 3, per_page: 1)
+    select "EU"
+
+    expect(page.current_path).to_not have_content("page=")
+  end
+
+  it "allows to clear all filters" do
+    cyfronet = create(:provider, name: "Cyfronet provider")
+    create(:service, title: "Other service")
+    create(:service, title: "Cyfronet service", providers: [cyfronet])
+
+    visit services_path(providers: [cyfronet.to_param])
+    expect(page).to_not have_text("Other service")
+
+    click_on "Clear all filters"
+
+    expect(page).to have_text("Other service")
+  end
+
+  it "shows active filters" do
+    platform = create(:platform)
+
+    visit services_path(related_platforms: [platform.id])
+    expect(page).to have_selector(".active-filters > *", count: 2)
+  end
+
+  context "multicheckbox" do
+    it "can be hierarchical" do
       root = create(:research_area)
       sub = create(:research_area, parent: root)
       subsub = create(:research_area, parent: sub)
@@ -18,7 +76,7 @@ RSpec.feature "Service filtering" do
       expect(body).to have_text(subsub.name)
     end
 
-    it "shows services from selected research area and sub research areas" do
+    it "show services from root and children when hierarchical" do
       root = create(:research_area)
       sub = create(:research_area, parent: root)
       subsub = create(:research_area, parent: sub)
@@ -45,6 +103,54 @@ RSpec.feature "Service filtering" do
       expect(page).to_not have_text("Sub service")
       expect(page).to have_text("Subsub service")
       expect(page).to_not have_text("Other service")
+    end
+
+    it "shows first 5 elements by default", js: true do
+      providers = create_list(:provider, 7)
+
+      visit services_path
+
+      expect(page).to have_text(providers[0].name)
+      expect(page).to have_text(providers[4].name)
+      expect(page).to_not have_text(providers[5].name)
+
+      expect(page).to have_text("Show 2 more")
+    end
+
+    it "can show more/less elements", js: true do
+      providers = create_list(:provider, 7)
+
+      visit services_path
+      click_on "Show 2 more"
+
+      expect(page).to have_text(providers[5].name)
+      expect(page).to have_text(providers[6].name)
+
+      click_on "Show less"
+      expect(page).to_not have_text(providers[5].name)
+      expect(page).to_not have_text(providers[6].name)
+    end
+
+    it "respect selected elements", js: true do
+      providers = create_list(:provider, 7)
+
+      visit services_path(providers: [providers[6].id])
+
+      expect(page).to_not have_text(providers[5].name)
+      expect(page).to have_text(providers[6].name)
+      expect(page).to have_text("Show 1 more")
+    end
+
+    it "respect selected child", js: true do
+      research_areas = create_list(:research_area, 7)
+      child = create(:research_area, parent: research_areas[6])
+
+      visit services_path(research_areas: [child.id])
+
+      expect(page).to_not have_text(research_areas[5].name)
+      expect(page).to have_text(research_areas[6].name)
+      expect(page).to have_text(child.name)
+      expect(page).to have_text("Show 1 more")
     end
   end
 
@@ -81,29 +187,31 @@ RSpec.feature "Service filtering" do
     end
   end
 
-  it "shows services with tag" do
-    create(:service, tag_list: ["a"], title: "ATag")
-    create(:service, tag_list: ["a", "b"], title: "ABTag")
-    create(:service, tag_list: ["c"], title: "CTag")
+  context "invisible filters" do
+    it "shows services with tag" do
+      create(:service, tag_list: ["a"], title: "ATag")
+      create(:service, tag_list: ["a", "b"], title: "ABTag")
+      create(:service, tag_list: ["c"], title: "CTag")
 
-    visit services_path(tag: "a")
-    expect(page).to have_text("ATag")
-    expect(page).to have_text("ABTag")
-    expect(page).to_not have_text("CTag")
+      visit services_path(tag: "a")
+      expect(page).to have_text("ATag")
+      expect(page).to have_text("ABTag")
+      expect(page).to_not have_text("CTag")
 
-    visit services_path(tag: ["a", "b"])
-    expect(page).to have_text("ATag")
-    expect(page).to have_text("ABTag")
-    expect(page).to_not have_text("CTag")
+      visit services_path(tag: ["a", "b"])
+      expect(page).to have_text("ATag")
+      expect(page).to have_text("ABTag")
+      expect(page).to_not have_text("CTag")
 
-    visit services_path(tag: ["a", "b", "c"])
-    expect(page).to have_text("ATag")
-    expect(page).to have_text("ABTag")
-    expect(page).to have_text("CTag")
+      visit services_path(tag: ["a", "b", "c"])
+      expect(page).to have_text("ATag")
+      expect(page).to have_text("ABTag")
+      expect(page).to have_text("CTag")
 
-    visit services_path(tag: ["d"])
-    expect(page).to_not have_text("ATag")
-    expect(page).to_not have_text("ABTag")
-    expect(page).to_not have_text("CTag")
+      visit services_path(tag: ["d"])
+      expect(page).to_not have_text("ATag")
+      expect(page).to_not have_text("ABTag")
+      expect(page).to_not have_text("CTag")
+    end
   end
 end
