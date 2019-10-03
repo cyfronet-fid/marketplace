166a99be320c86ccc73d2f5d3bda118716a00d87
Marta Świątkowska <martaswiatkowska8@gmail.com>
2019-10-03 16:12:59 +0200

    Fix services iid for project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_090322) do
+ActiveRecord::Schema.define(version: 2019_10_03_120324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 2019_09_19_090322) do
     t.string "department"
     t.string "webpage"
     t.string "status"
-    t.datetime "created_at", default: "2019-09-19 09:11:52", null: false
-    t.datetime "updated_at", default: "2019-09-19 09:11:52", null: false
+    t.datetime "created_at", default: "2019-10-09 08:57:14", null: false
+    t.datetime "updated_at", default: "2019-10-09 08:57:14", null: false
     t.index ["name", "user_id"], name: "index_projects_on_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
