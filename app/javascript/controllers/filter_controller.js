85efb67dad6e1c82637eb037190ca3c238b445c9
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-10-10 14:40:15 +0200

    Merge filters and filter stimulus controller into one

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,10 +1,16 @@
 import {Controller} from 'stimulus'
 
 export default class extends Controller {
+  static targets = ['form']
+
   toggle(event) {
     const target = event.currentTarget;
     const collapsed = !target.classList.contains("collapsed");
 
     document.cookie = `${target.id}=${collapsed}`;
+  }
+
+  reload(event) {
+    this.formTarget.submit();
   }
 }
