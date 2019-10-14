385e3ab9a61a9f23ffc0641d4227d481e4ad7b1f
Marek Kasztelnik <mkasztelnik@gmail.com>
2019-10-14 13:12:37 +0200

    Clean service browsing specs
    Deep cleanup for all specs related to service browsing. Specs related to
    concrete features (e.g. search, filter, ask a question) were moved to
    dedicated files. Some tests which test case was duplicated by other
    tests were removed. Also all `sleep` was eliminated from the spec code.
    This was possible thanks to this simple trick:
    
    https://stackoverflow.com/questions/41854537/capybara-poltergeist-wait-for-javascript-completions

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature "Service searching in top bar", js: true do
 
   before { category = create(:category) }
 
-  scenario "search with 'Allservices' selected should submit to /services" do
+  scenario "search with 'All Services' selected should submit to /services" do
     visit root_path
     select "All services", from: "category-select"
     click_on(id: "query-submit")
@@ -32,5 +32,37 @@ RSpec.feature "Service searching in top bar", js: true do
     expect(url.path).to eq(category_services_path(category_id: category))
 
     expect(page).to have_select("category-select", selected: category.name)
+  end
+
+  scenario "I can clear search conditions" do
+    visit services_path(q: "DDDD Something")
+    expect(page).to have_css(".categories", text: "Looking for: DDDD Something")
+    find(:css, ".search-clear").click
+    expect(page).to_not have_css(".categories", text: "Looking for: DDDD Something")
+  end
+
+  scenario "redirect when selecting service_id by autocomplete controller", js: true, search: true do
+    service = create(:service)
+    fill_in "q", with: service.title
+    find(:css, "li.dropdown-item[id='-option-0']").click
+    expect(current_path).to eq(service_path(service))
+  end
+
+  scenario "redirect when selecting service_id by autocomplete controller", js: true, search: true do
+    service = create(:service)
+    visit services_path(service_id: service.id)
+    expect(current_path).to eq(service_path(service))
+  end
+
+  scenario "After starting searching autocomplete are shown", js: true, search: true do
+    create(:service, title: "DDDD Something 1")
+    create(:service, title: "DDDD Something 2")
+    create(:service, title: "DDDD Something 3")
+
+    visit services_path
+
+    fill_in "q", with: "DDDD Something"
+
+    expect(page).to have_selector("li.dropdown-item[role='option']:not([style*=\"display: none\"]", count: 3)
   end
 end
