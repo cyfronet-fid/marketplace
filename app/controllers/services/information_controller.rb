160febbd990c928dbfe2deb8f64864f1facb5afe
Marta Świątkowska <martaswiatkowska8@gmail.com>
2019-11-06 14:50:16 +0100

    Anonymous yuser can enter information step

--- app/controllers/services/information_controller.rb
+++ app/controllers/services/information_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Services::InformationController < Services::ApplicationController
+  skip_before_action :authenticate_user!
   before_action :ensure_in_session!
 
   def show
