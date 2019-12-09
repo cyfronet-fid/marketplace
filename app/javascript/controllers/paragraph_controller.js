447434ef59b5e8ba0c1e2075cf7df056e19068b4
Jaroslaw Zbozien <j.zbozien@gmail.com>
2019-12-09 11:56:01 +0100

    access information page view

--- app/javascript/controllers/paragraph_controller.js
+++ app/javascript/controllers/paragraph_controller.js
@@ -3,11 +3,6 @@ import {Controller} from 'stimulus'
 export default class extends Controller {
   static targets = ["showMore"];
 
-  connect() {
-  }
-
-  initialize(){}
-
   showMore(event){
     const element = event.target
     event.preventDefault()
