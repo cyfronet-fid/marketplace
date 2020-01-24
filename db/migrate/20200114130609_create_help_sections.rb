d6df1c97ebca99b03d46bd688e0ac986c767a82d
Marek Kasztelnik <mkasztelnik@gmail.com>
2020-01-24 12:35:14 +0100

    Fix migration with correct not null declaration

--- db/migrate/20200114130609_create_help_sections.rb
+++ db/migrate/20200114130609_create_help_sections.rb
@@ -1,9 +1,9 @@
 class CreateHelpSections < ActiveRecord::Migration[6.0]
   def change
     create_table :help_sections do |t|
-      t.string :title, nil: false
+      t.string :title, null: false
       t.string :slug, unique: true
-      t.integer :position, nil: false, default: 0
+      t.integer :position, null: false, default: 0
 
       t.timestamps
     end
--- db/migrate/20200114134749_create_help_items.rb
+++ db/migrate/20200114134749_create_help_items.rb
@@ -1,9 +1,9 @@
 class CreateHelpItems < ActiveRecord::Migration[6.0]
   def change
     create_table :help_items do |t|
-      t.string :title, nil: false
+      t.string :title, null: false
       t.string :slug, unique: true
-      t.integer :position, nil: false, default: 0
+      t.integer :position, null: false, default: 0
 
       t.belongs_to :help_section, null: false
 
