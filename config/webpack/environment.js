7d3695286eb4d11a1afbc81880f056657116fb2f
Michał Szostak <michal.szostak@cyfronet.pl>
2020-02-13 15:42:42 +0100

    Custom SCSS loader for customization
    * Add custom loader which prioritizes files from CUSTOMIZATION_PATH if
      it's set

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
-const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
+const {environment} = require('@rails/webpacker');
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path');
 
 /**
  * Automatically load modules instead of having to import or require them
@@ -10,25 +12,81 @@ const webpack = require('webpack')
  * http://j.mp/2JzG1Dm
  */
 environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+    })
+);
 
 /**
  * To use jQuery in views
  */
 environment.loaders.append('expose', {
-  test: require.resolve('jquery'),
-  use: [{
-    loader: 'expose-loader',
-    options: '$'
-  }]
+    test: require.resolve('jquery'),
+    use: [{
+        loader: 'expose-loader',
+        options: '$'
+    }]
 })
 
-module.exports = environment
+const CUSTOMIZATION_PATH = process.env.CUSTOMIZATION_PATH;
+
+if (CUSTOMIZATION_PATH) {
+    const rootDir = path.resolve('.');
+    const relRE = new RegExp(`${rootDir}/app/(.+)/.+\..+`);
+    const sassLoader = environment.loaders.get('sass').use.find(element => element.loader === 'sass-loader');
+
+    sassLoader.options.importer = function (url, prev, done) {
+        if (url.startsWith('~')) {
+            return null;
+        }
+
+        const match = prev.match(relRE);
+        if (!match) {
+            return null;
+        }
+
+        const relPath = match[1];
+
+        function findPath(path, url) {
+            function checkAndReturn(path) {
+                if(fs.existsSync(path)) {
+                    return path
+                }
+                return null;
+            }
+
+            const check = checkAndReturn(path + '/' + url)
+                || checkAndReturn(path + '/' + url + '.scss')
+                || checkAndReturn(path + '/' + url + '.sass');
+
+            if(check) {return check;}
+
+            const pos = url.search(new RegExp('[^/\.]\..+$'));
+            if(pos === -1 || url.charAt(1) === '_') {
+                return null;
+            }
+
+            const urlSub = url.slice(0, pos) + '_' + url.slice(pos);
+            return checkAndReturn(path + '/' + urlSub)
+            || checkAndReturn(path + '/' + urlSub + '.scss')
+            || checkAndReturn(path + '/' + urlSub + '.sass');
+        }
+
+
+        const filePath = findPath(CUSTOMIZATION_PATH + '/' + relPath, url);
+
+        if (filePath) {
+            return {file: filePath};
+        }
+
+        return null;
+    };
+
+}
+
+module.exports = environment;
