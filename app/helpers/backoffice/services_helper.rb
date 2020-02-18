0105cd7b8616920baeed405a2267445bab38659b
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-02-18 16:42:40 +0100

    [#945] Information about offers statuses in the backoffice
    Add information in the services index
    and services show about offers statuses

--- app/helpers/backoffice/services_helper.rb
+++ app/helpers/backoffice/services_helper.rb
@@ -11,6 +11,14 @@ module Backoffice::ServicesHelper
     content_tag(:span, service.status, class: "badge #{BADGES[service.status]}")
   end
 
+  def offers_status(service)
+    if service.offers.blank?
+      content_tag(:span, "NO OFFERS", class: "badge badge-error")
+    elsif service.published? && service.offers.published.blank?
+      content_tag(:span, "NO PUBLISHED OFFERS", class: "badge badge-warning")
+    end
+  end
+
   def offer_status(offer)
     content_tag(:span, offer.status, class: "badge #{BADGES[offer.status]}")
   end
