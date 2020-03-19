0b342c19c3c8ea426da1d50787ead25fa2d3970b
Marek Kasztelnik <mkasztelnik@gmail.com>
2020-03-19 12:28:23 +0100

    Rename AttributesHelper to ParametersHelper

--- app/helpers/backoffice/parameters_helper.rb
+++ app/helpers/backoffice/parameters_helper.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-module Backoffice::AttributesHelper
-  def attribute_templates(form)
+module Backoffice::ParametersHelper
+  def parameter_templates(form)
     content_tag(:div, class: "dropdown") do
       concat content_tag(:button, "Add new parameter",
                          class: "btn btn-success dropdown-toggle", type: "button",
                          "data-toggle": "dropdown", "aria-haspopup": true,
                          "aria-expanded": false, id: "attributes-list-button")
-      concat content_tag(:div, attributes_menu_items(form),
+      concat content_tag(:div, parameter_menu_items(form),
                          class: "dropdown-menu",
                          "aria-labelledby": "attributes-list-button")
     end
   end
 
-  def attributes_menu_items(form)
+  def parameter_menu_items(form)
     capture do
       Parameter.all.map do |clazz|
-        concat content_tag(:a, I18n.t("attributes.template.#{clazz.type}.add"),
+        concat content_tag(:a, I18n.t("properties.#{clazz.type}.add"),
                            class: "dropdown-item",
-                           "data-template": attribute_template(clazz.new(id: "js_template_id"), form),
+                           "data-template": parameter_template(clazz.new(id: "js_template_id"), form),
                            "data-action": "click->offer#addAttribute")
       end
     end
   end
 
-  def attribute_template(attr, form)
+  def parameter_template(parameter, form)
     render(partial: "parameters/template",
-           locals: { form: form, attribute: attr }).html_safe
+           locals: { form: form, parameter: parameter }).html_safe
   end
 end
