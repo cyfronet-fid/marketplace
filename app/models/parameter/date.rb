3ddb33989a934ee12644dd8b1981873957d60cfa
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-03-20 12:25:56 +0100

    Styling for date input parameter

--- app/models/parameter/date.rb
+++ app/models/parameter/date.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 class Parameter::Date < Parameter
-  attr_accessor :min, :max
+  def dump
+    ActiveSupport::HashWithIndifferentAccess.new(id: id,
+                                                 type: "date",
+                                                 label: name,
+                                                 description: hint,
+                                                 value_type: "string")
+  end
+
+  def self.load(hsh)
+    new(id: hsh["id"], name: hsh["label"], hint: hsh["description"])
+  end
 end
