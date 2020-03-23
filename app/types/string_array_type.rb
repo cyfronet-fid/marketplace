f272ec9add6dee48593355481825575ab08e96e3
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-03-23 16:06:07 +0100

    Select parameter

---
+++ app/types/string_array_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class StringArrayType < ActiveModel::Type::Value
+  attr_reader :delimiter
+
+
+  def initialize(delimiter = ",")
+    @delimiter = delimiter
+  end
+
+  def cast(value)
+    if value.is_a?(::String)
+      value = value.split(@delimiter)
+    end
+    value
+  end
+end
---
+++ config/initializers/types.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+ActiveModel::Type.register(:string_array, StringArrayType)
