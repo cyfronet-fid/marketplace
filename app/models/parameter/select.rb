7670a53d1f31196365308ce4fc976310445f73b3
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-03-23 16:06:07 +0100

    Select parameter

--- app/models/parameter/select.rb
+++ app/models/parameter/select.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
 class Parameter::Select < Parameter
-  attr_accessor :values
+  attribute :values, :string_array
+  attribute :mode, :string
+
+
+  validates :mode, presence: true, inclusion: %w[dropdown buttons]
+  validates :values, presence: true
+
+
+  def dump
+    ActiveSupport::HashWithIndifferentAccess.new(
+      id: id, type: "select", label: name, description: hint,
+      config: { values: values, mode: mode }, value_type: "string")
+  end
+
+  def self.load(hsh)
+    new(id: hsh["id"], name: hsh["label"], hint: hsh["description"],
+        values: hsh.dig("config", "values"), mode: hsh.dig("config", "mode"))
+  end
 end
