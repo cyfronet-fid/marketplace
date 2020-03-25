ba0aa9b94ea251b32572ceb2936db82227f2ecac
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-03-25 17:11:24 +0100

    Multiselect parameter

--- app/models/parameter/multiselect.rb
+++ app/models/parameter/multiselect.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class Parameter::Multiselect < Parameter
-  attr_accessor :values, :min, :max
-end
+  attribute :values, :string_array
+  attribute :min, :integer, default: 2
 
+  validates :values, presence: true
+  validates :min, presence: true
+  validate do
+    if min && values && min >= values.length
+      errors.add(:min, "must be less than number of items")
+    end
+  end
+
+  def dump
+    ActiveSupport::HashWithIndifferentAccess.new(
+      id: id, type: "multiselect", label: name, description: hint,
+      config: { values: values, minItems: min }, value_type: "string")
+  end
+
+  def self.load(hsh)
+    new(id: hsh["id"], name: hsh["label"], hint: hsh["description"],
+        values: hsh.dig("config", "values"), min: hsh.dig("config", "minItems"))
+  end
+end
