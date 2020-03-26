1aa89ec1751f3e53008b672e2206983658b26d4d
Marek Kasztelnik <mkasztelnik@gmail.com>
2020-03-26 14:38:55 +0100

    Parameter serialization specs

---
+++ spec/models/parameter/select_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Parameter::Select do
+  subject { build(:select_parameter) }
+
+  it "serializes to attribute schema" do
+    expect(Attribute.from_json(subject.dump)).to be_config_valid
+  end
+
+  it "serializes to valid attribute attribute" do
+    attribute = Attribute.from_json(subject.dump)
+    attribute.value_from_param("b")
+
+    expect(attribute).to be_value_valid
+  end
+end
