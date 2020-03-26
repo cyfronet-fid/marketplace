43dbcad6478cd3b90647eb03aa3d24aac6a3b089
Marek Kasztelnik <mkasztelnik@gmail.com>
2020-03-26 14:38:55 +0100

    Parameter serialization specs

---
+++ spec/models/parameter/date_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Parameter::Date do
+  subject { build(:date_parameter) }
+
+  it "serializes to attribute schema" do
+    expect(Attribute.from_json(subject.dump)).to be_config_valid
+  end
+
+  it "serializes to valid attribute attribute" do
+    attribute = Attribute.from_json(subject.dump)
+    attribute.value_from_param(I18n.l(Date.today))
+
+    expect(attribute).to be_value_valid
+  end
+end
---
+++ spec/models/parameter/quantity_price_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Parameter::Input do
+  subject { build(:quantity_price_parameter) }
+
+  it "serializes to attribute schema" do
+    expect(Attribute.from_json(subject.dump)).to be_config_valid
+  end
+
+  it "serializes to valid attribute attribute" do
+    attribute = Attribute.from_json(subject.dump)
+    attribute.value_from_param("7")
+
+    expect(attribute).to be_value_valid
+  end
+end
