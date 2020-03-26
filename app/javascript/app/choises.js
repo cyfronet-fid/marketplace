b42ed2a5f276fe5dd7e6c409881298c3fe924f38
Marek Kasztelnik <mkasztelnik@gmail.com>
2020-03-26 10:53:31 +0100

    Initialize choices.js after select is added

--- app/javascript/app/choises.js
+++ app/javascript/app/choises.js
@@ -1,10 +1,13 @@
 import Choices from 'choices.js'
 
-export default function initChoises() {
-  new Choices('[data-choice]', {
-    removeItems: true,
-    placeholder: true,
-    placeholderValue: '+ Add',
-    removeItemButton: true,
+export default function initChoises(scope = document) {
+  const elements = scope.querySelectorAll('[data-choice]');
+  elements.forEach(function(element) {
+    new Choices(element, {
+      removeItems: true,
+      placeholder: true,
+      placeholderValue: '+ Add',
+      removeItemButton: true,
+    });
   });
 }
