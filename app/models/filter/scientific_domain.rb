25fedcec3944057cc3de2fdcb10a5f4204710de6
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-06-09 21:00:53 +0200

    [#1538] Rename research_area to scientific_domain
    Frontend refactoring

--- app/models/filter/scientific_domain.rb
+++ app/models/filter/scientific_domain.rb
@@ -3,7 +3,7 @@
 class Filter::ScientificDomain < Filter::AncestryMultiselect
   def initialize(params = {})
     super(params: params.fetch(:params, {}),
-          field_name: "scientific_domains", title: "Scientific Domain",
+          field_name: "scientific_domains", title: "Scientific Domains",
           model: ::ScientificDomain, joining_model: ServiceScientificDomain,
           index: "scientific_domains", search: true)
   end
