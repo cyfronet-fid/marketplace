e001ba5b63a9e146d6ae58ef8f5b9956f06415c8
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-06-17 17:44:49 +0200

    [#1554] Add new url fields in service model
    Add new url fields to the service model:
    - status_monitoring_url
    - maintenance_url
    - order_url
    - payment_model_url
    - pricing_url
    Fixex #1554

---
+++ db/migrate/20200617145432_add_new_url_fields_to_service.rb
@@ -0,0 +1,9 @@
+class AddNewUrlFieldsToService < ActiveRecord::Migration[6.0]
+  def change
+    add_column :services, :status_monitoring_url, :string
+    add_column :services, :maintenance_url, :string
+    add_column :services, :order_url, :string
+    add_column :services, :payment_model_url, :string
+    add_column :services, :pricing_url, :string
+  end
+end
