674addf3ccc3cbeff524cc489f445225b19e7cf2
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-06-23 14:18:58 +0200

    Add parsing to migration

--- db/migrate/20200615171221_change_places_to_geographical_availabilities.rb
+++
@@ -1,5 +0,0 @@
-class ChangePlacesToGeographicalAvailabilities < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :services, :places, :geographical_availabilities
-  end
-end
---
+++ db/migrate/20200624085709_change_places_to_geographical_availabilities.rb
@@ -0,0 +1,16 @@
+class ChangePlacesToGeographicalAvailabilities < ActiveRecord::Migration[6.0]
+  def change
+    rename_column :services, :places, :geographical_availabilities
+      execute(
+        <<~SQL
+          UPDATE services
+          SET geographical_availabilities[1] =
+            CASE geographical_availabilities[1]
+              WHEN 'Europe' THEN 'EU'
+              WHEN 'World' THEN 'WW'
+              WHEN 'Worldwide' THEN 'WW'
+            END
+        SQL
+      )
+  end
+end
