3089b56e9d1958da251f892b103307adfe1a26a5
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-06-23 14:18:58 +0200

    Add parsing to migration

--- app/helpers/service_helper.rb
+++ app/helpers/service_helper.rb
@@ -81,8 +81,9 @@ module ServiceHelper
 
   def data_for_map(geographical_availabilities)
     countries = []
+
     geographical_availabilities.each { |place|
-      c = Country.countries_for_region(place&.name)
+      c = Country.countries_for_region(place.name) if place
       c = [place] if c.blank?
       countries = countries | c
     }
