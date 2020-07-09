5cb731d620f8d7e34417ce29271a89423950faf4
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-07-09 15:12:06 +0200

    [#1581] Add missing fields in service schema
    Add new fields for the new schema:
    - resource_geographic_locations(multiselect)
    - certifications(multiselect)
    - standards(multiselect)
    - open_source_technologies(multiselect)
    - grant_project_names(multiselect)
    - changelog(string)
    - last_update(datetime)
    
    Add new associations based on vocabularies:
    - access_types
    - access_modes

---
+++ spec/factories/access_mode.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :access_mode do
+    sequence(:name) { |n| "access mode #{n}" }
+    sequence(:eid) { |n| "access_mode-#{n}" }
+    sequence(:description) { |n| "Description #{n}" }
+    sequence(:extras) { |n| {} }
+  end
+end
