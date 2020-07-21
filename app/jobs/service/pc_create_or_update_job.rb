d2fa2caf75e16b1e23ec689aaa74ab09077e769f
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-07-21 12:33:45 +0200

    Restore service validation

--- app/jobs/service/pc_create_or_update_job.rb
+++ app/jobs/service/pc_create_or_update_job.rb
@@ -3,6 +3,10 @@
 class Service::PcCreateOrUpdateJob < ApplicationJob
   queue_as :jms
 
+  rescue_from(Errno::ECONNREFUSED) do |exception|
+    raise exception
+  end
+
   def perform(infra_service, eic_base_url, is_active)
     Service::PcCreateOrUpdate.new(infra_service,
                                           eic_base_url,
