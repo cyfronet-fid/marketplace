7ab48cd85bc155fdb5b644bc8eed64069c51d0bc
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-08-07 15:27:00 +0200

    Placeholder for split

---
+++ config/initializers/split.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+cookie_adapter = Split::Persistence::CookieAdapter
+redis_adapter = Split::Persistence::RedisAdapter.with_config(
+    lookup_by: -> (context) { context.request.env['warden']&.user&.id },
+    expire_seconds: 2592000)
+
+Split.configure do |config|
+  config.persistence = Split::Persistence::DualAdapter.with_config(
+      logged_in: -> (context) { context.request.env['warden']&.user&.id.present? },
+      logged_in_adapter: redis_adapter,
+      logged_out_adapter: cookie_adapter)
+  config.persistence_cookie_length = 2592000 # 30 days
+  config.experiments = YAML.load_file "config/experiments.yml"
+  config.allow_multiple_experiments = true
+end
