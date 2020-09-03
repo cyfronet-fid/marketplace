e3d8703277fe3ce8f2cd7130375f704807c8bd13
Marta Świątkowska <martaswiatkowska8@gmail.com>
2020-09-03 09:28:50 +0200

    [#1653] Multipage tours

--- app/helpers/tour_helper.rb
+++ app/helpers/tour_helper.rb
@@ -7,34 +7,72 @@ module TourHelper
     tours ||= Rails.configuration.tours.list["#{controller_name}.#{action_name}.#{I18n.default_locale}"]
 
     if tours
-      remaining = tours.keys - finished_tours.map(&:tour_name)
-
-      if remaining.any? && !cookies["#{cookie_prefix("unlogged")}-#{remaining.first}"]
+      remaining = tours.keys - finished_tours
+      to_show = tours.select { |t| show_tour?(t, tours[t]) }.keys
+      to_render = to_show & remaining
+      unless to_render.empty?
         render(partial: "layouts/tour",
-        locals: { tour_name: remaining.first,
-        steps: tours[remaining.first]["steps"] })
+               locals: { tour_name: to_render.first,
+               steps: tours[to_render.first]["steps"],
+               next_tour: further_tour(tours[to_render.first]) })
       end
     end
   end
 
-  def finished_tours
-    TourHistory.where(
-      user_id: current_user&.id,
-      action_name: action_name,
-      controller_name: controller_name
-    )
+  def finished_tours(controller = controller_name, action = action_name)
+    user_cookie(controller, action) ? JSON.parse(user_cookie(controller, action)) : []
   end
 
-  def finished?
-    TourHistory.where(
-      user_id: current_user.id,
-      controller_name: controller_name,
-      action_name: action_name
-    ).empty?
+  def finished?(tour_name, controller =  controller_name, action =  action_name)
+    user_cookie(controller, action) ? JSON.parse(user_cookie(controller, action)).include?(tour_name) : false
   end
 
-  def cookie_prefix(user_id = current_user ? current_user.id : "unlogged")
-    "tours-mp-#{user_id}-#{controller_name}-#{action_name}"
+  def show_tour?(tour_name, tour)
+    if last_part_of_tour?(tour)
+      finished?(tour.dig("previous", "tour_name"),
+                tour.dig("previous", "controller_name"),
+                tour.dig("previous", "action_name"))
+    elsif first_part_of_tour?(tour)
+      !finished?(tour.dig("next", "tour_name"),
+                 tour.dig("next", "controller_name"),
+                 tour.dig("next", "action_name"))
+    elsif tour.key?("previous") && tour.key?("next")
+      if finished?(tour.dig("next", "tour_name"),
+                   tour.dig("next", "controller_name"),
+                   tour.dig("next", "action_name"))
+        false
+      else
+        finished?(tour.dig("previous", "tour_name"),
+                  tour.dig("previous", "controller_name"),
+                  tour.dig("previous", "action_name"))
+      end
+    else
+      true
+    end
+  end
+
+  def user_cookie(controller = controller_name, action = action_name)
+    cookies["#{cookie_prefix(controller, action)}-completed"]
+  end
+
+  def further_tour(tour)
+    tour.dig("next", "redirect_to")
+  end
+
+  def next_tour_link(next_tour_path)
+    next_tour_path.blank? ? nil : send(next_tour_path).to_s
+  end
+
+  def cookie_prefix(controller = controller_name, action = action_name)
+    "tours-marketplace-#{controller}-#{action}"
+  end
+
+  def last_part_of_tour?(tour)
+    tour.key?("previous") && !tour.key?("next")
+  end
+
+  def first_part_of_tour?(tour)
+    !tour.key?("previous") && tour.key?("next")
   end
 
   def tours_domain
