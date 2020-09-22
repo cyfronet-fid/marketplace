24bfa2a28af8e14f3fafa0dcf761a0c33a30243c
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-09-22 15:46:22 +0200

    Split pc and internal platforms and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_154501) do
+ActiveRecord::Schema.define(version: 2020_09_22_120654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -406,6 +406,8 @@ ActiveRecord::Schema.define(version: 2020_09_16_154501) do
     t.string "privacy_policy_url"
     t.string "use_cases_url", default: [], array: true
     t.datetime "last_update"
+    t.string "related_platforms", default: [], array: true
+    t.datetime "synchronized_at"
     t.index ["name"], name: "index_services_on_name"
     t.index ["provider_id"], name: "index_services_on_provider_id"
     t.index ["resource_organisation_id"], name: "index_services_on_resource_organisation_id"
