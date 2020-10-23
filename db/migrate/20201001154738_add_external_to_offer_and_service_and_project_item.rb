3826441447b8fc9cb83f82588499d995afe06fb4
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-10-23 12:15:28 +0200

    Add internal field to offers and project_items

--- db/migrate/20201001154738_add_external_to_offer_and_service_and_project_item.rb
+++
@@ -1,7 +0,0 @@
-class AddExternalToOfferAndServiceAndProjectItem < ActiveRecord::Migration[6.0]
-  def change
-    add_column :offers, :external, :boolean, default: false
-    add_column :services, :external, :boolean, default: false
-    add_column :project_items, :external, :boolean, default: false
-  end
-end
---
+++ db/migrate/20201001154738_add_internal_to_offer_and_project_item.rb
@@ -0,0 +1,6 @@
+class AddInternalToOfferAndProjectItem < ActiveRecord::Migration[6.0]
+  def change
+    add_column :offers, :internal, :boolean, default: false
+    add_column :project_items, :internal, :boolean, default: false
+  end
+end
