150d94097ba875f1849fdf373a4d083687e4619a
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-11-09 18:42:03 +0100

    Add `remove array element` in forms
    Also
    Fixes #1690

--- app/inputs/array_input.rb
+++ app/inputs/array_input.rb
@@ -5,14 +5,24 @@ class ArrayInput < SimpleForm::Inputs::TextInput
     input_html_options[:type] ||= input_type
     existing_value = Array(object.public_send(attribute_name)).map.with_index do |array_el, index|
       @builder.text_area(nil, input_html_options.merge(value: array_el,
-                                                        name: "#{object_name}[#{attribute_name}][]",
-                                                        id: "#{object_name}_#{attribute_name}_#{index}"))
+                                                       name: "#{object_name}[#{attribute_name}][]",
+                                                       id: "#{object_name}_#{attribute_name}_#{index}")) +
+          template.content_tag(:a, "Remove", id: "remove_#{object_name}_#{attribute_name}_#{index}",
+                               class: "btn btn-danger",
+                               "data-target": "#{attribute_name}",
+                               "data-action": "click->service#removeField",
+                               "data-value": "#{object_name}_#{attribute_name}_#{index}")
     end
     number = Array(object.public_send(attribute_name)).length
     existing_value.push @builder.text_area(nil,
-                                             input_html_options.merge(value: nil,
-                                                                      name: "#{object_name}[#{attribute_name}][]",
-                                                                      id: "#{object_name}_#{attribute_name}_#{number}"))
+                                           input_html_options.merge(value: nil,
+                                           name: "#{object_name}[#{attribute_name}][]",
+                                           id: "#{object_name}_#{attribute_name}_#{number}")) +
+                        template.content_tag(:a, "Remove", id: "remove-#{object_name}_#{attribute_name}_#{number}",
+                                             class: "btn btn-danger",
+                                             "data-target": "#{attribute_name}",
+                                             "data-action": "click->service#removeField",
+                                             "data-value": "#{object_name}_#{attribute_name}_#{number}")
     existing_value.join.html_safe
   end
 
