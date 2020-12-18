8dd62cf7e77e9fe811d994a74095d88e3faaf831
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2020-12-18 15:43:39 +0100

    [#1713] Create data administraotr model for providers
    This is preparation for build API for providers
    Add data administrators model for providers

---
+++ db/migrate/20201216160453_create_data_administrators.rb
@@ -0,0 +1,11 @@
+class CreateDataAdministrators < ActiveRecord::Migration[6.0]
+  def change
+    create_table :data_administrators do |t|
+      t.string :first_name, index: true
+      t.string :last_name, index: true
+      t.string :email, null: false, index: true
+
+      t.timestamps
+    end
+  end
+end
