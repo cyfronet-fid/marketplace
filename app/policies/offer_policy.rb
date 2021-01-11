40b63b0801c01a0198b3898cf108644123a6f511
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2021-01-11 14:31:06 +0100

    [IN PROGRESS] Add policies and views to data_administrator
    Views are available in services/:id/data_adminitrator

--- app/policies/offer_policy.rb
+++ app/policies/offer_policy.rb
@@ -10,4 +10,27 @@ class OfferPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def edit?
+    offer_editor?(user)
+  end
+
+  def new?
+    offer_editor?(user)
+  end
+
+  def update?
+    offer_editor?(user)
+  end
+
+  def create?
+    offer_editor?(user)
+  end
+
+  private
+    def offer_editor?(user)
+      DataAdministrator.where(email: user&.email).count.positive? ||
+        user&.service_portfolio_manager? ||
+        user&.service_owner?
+    end
 end
