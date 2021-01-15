41dfd832e817576d4c3f189b1a276f0b6bf09375
Krzysztof Marszałek <marszalekk@gmail.com>
2021-01-15 11:36:30 +0100

    [#1711] Fix filtering by country
    - include regions for country
    - sort input for select

--- app/models/filter/location.rb
+++ app/models/filter/location.rb
@@ -4,7 +4,7 @@ class Filter::Location < Filter
   def initialize(params = {})
     super(params: params.fetch(:params, {}),
           field_name: "geographical_availabilities",
-          title: "Provider location",
+          title: "Resource availability",
           type: :select,
           index: "geographical_availabilities")
   end
@@ -12,10 +12,10 @@ class Filter::Location < Filter
   private
     def fetch_options
       [{ name: "Any", id: "" }] + Service.where(status: [:published, :unverified])
-             .pluck(:geographical_availabilities).flatten.uniq.map { |s| { name: s, id: s } }
+             .pluck(:geographical_availabilities).flatten.uniq.sort.map { |s| { name: s, id: s } }
     end
 
     def where_constraint
-      { @index.to_sym => values.map { |c| Country.dump(Country.find_by_name(c)) } }
+      { @index.to_sym => Country.convert(value) }
     end
 end
