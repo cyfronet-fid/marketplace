47009a426b92399fed82f0d3285324289e7e90bc
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2021-02-26 17:57:39 +0100

    [#1797] Migrate `unverified` services to `published` status
    Add migration for changing status value
    Change default in importers mapping
    Fixes #1797

---
+++ db/migrate/20210226164811_migrate_services_to_published_status.rb
@@ -0,0 +1,11 @@
+class MigrateServicesToPublishedStatus < ActiveRecord::Migration[6.0]
+  def change
+    execute(
+      <<~SQL
+      UPDATE services
+      SET status = 'published'
+      WHERE status = 'unverified';
+    SQL
+    )
+  end
+end
