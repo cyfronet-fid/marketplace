d6bf8364478c28bb490693447987e2bbce059d1b
Daniel Kryska <daniel.kryska94@gmail.com>
2021-03-09 09:29:41 +0100

    [#1782] Fixes of probation system and extend filters gathering for services recommendations
    Fixes of probation system
    - Remove multiple triggering on [data-probe] single click
    - Add attributes to not mentioned items in 'Recommendation's probe placement ...' confluence document to keep consistnecy
    - Track user typing in catalog search, add listener to it
    
    Filters gathering extension for services recommendations
    - Add query value (category_id) and all query params that are filters

--- spec/features/recommended_spec.rb
+++ spec/features/recommended_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "rails_helper"
+require "raven"
 
 RSpec.feature "Recommended services" do
   include OmniauthHelper
@@ -13,10 +14,27 @@ RSpec.feature "Recommended services" do
 
     services_ids = [1, 2, 3]
     services_ids.each { |id| create(:service, id: id) }
-    allow(Unirest).to receive(:post).and_return(services_ids)
+    allow(Unirest).to receive(:post).and_return({ "recommendations": services_ids })
+    expect(Recommender::SimpleRecommender).not_to receive(:new)
 
     visit services_path
+    expect(all("[data-probe=\"recommendation-panel\"]").length).to be services_ids.length
+  end
 
-    expect(all("[data-probe='recommendation-panel']").length).to be services_ids.length
+  it "should display recommended on unirest error", js: true do
+    use_ab_test(recommendation_panel: "v1")
+    allow(Mp::Application.config).to(
+      receive(:recommender_host).and_return("localhost:5000")
+    )
+
+    allow(Unirest).to receive(:post).and_raise(ArgumentError)
+    allow(Raven).to receive(:capture_message)
+
+    services_ids = [1, 2, 3]
+    services = services_ids.map { |id| create(:service, id: id) }
+    allow(Recommender::SimpleRecommender).to receive_message_chain(:new, :call).and_return(services)
+
+    visit services_path
+    expect(all("[data-probe='recommendation-panel']").length).not_to eq 0
   end
 end
