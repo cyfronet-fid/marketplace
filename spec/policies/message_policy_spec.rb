d8049503fbe547cc0d654fc30044ff9e8b91bd52
Jakub Sawicki <jswk@users.noreply.github.com>
2021-03-31 12:12:04 +0200

    Add author_role and scope to Message
    The author_role extends the Message typology, so that Messages
    which come from providers and ops-team can be differentiated
    (author_role=user|provider|mediator).
    On the other hand, scope allows the providers and ops-team to exchange
    Messages private to user between OMSes (scope=internal) and allows
    providers (or ops-team in projects) to send Messages privately
    to user (scope=user_direct).
    
    Before, when a Message wasn't sent by an MP user, it only had the
    author_id==nil. I take it into account when migrating data.
    
    Yet unused, but add author_name and author_email columns, so that
    OMSes can provide them when creating messages, making it easier for user
    and other participating providers and ops-team members to discern the
    Message origin.
    
    Update the conversation_controllers, so that only public_scope? and
    user_direct_scope? are shown to users. Also, make sure the messages are
    always ordered by creation time (for project_items the order was by
    updated time, inconsitent with the project level).

---
+++ spec/policies/message_policy_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe MessagePolicy, type: :policy do
+  let(:user) { create(:user) }
+
+  subject { described_class }
+
+  def resolve
+    subject::Scope.new(user, Message).resolve
+  end
+
+  permissions ".scope" do
+    it "allows scope=public" do
+      message = create(:message, scope: :public)
+
+      expect(resolve).to contain_exactly(message)
+    end
+
+    it "allows scope=user_direct" do
+      message = create(:message, scope: :user_direct)
+
+      expect(resolve).to contain_exactly(message)
+    end
+
+    it "forbids scope=internal" do
+      create(:message, scope: :internal)
+
+      expect(resolve).to be_empty
+    end
+  end
+
+  context "permitted_attributes" do
+    it "should return message" do
+      policy = described_class.new(user, create(:message))
+      expect(policy.permitted_attributes).to match_array([:message])
+    end
+  end
+end
