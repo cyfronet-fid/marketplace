cb9967509ba1dcab84c49bf2fd1230d4898630d3
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2021-04-01 13:56:51 +0200

    [HOTFIX] Fix set upstream for providers
    Fixes REST and JMS importers should set upstream for providers
    exactly like it is in the services

--- app/services/provider/pc_create_or_update.rb
+++ app/services/provider/pc_create_or_update.rb
@@ -8,20 +8,23 @@ class Provider::PcCreateOrUpdate
   end
 
   def call
-    prov = Importers::Provider.new(@eic_provider, @modified_at).call
+    provider_hash = Importers::Provider.new(@eic_provider, @modified_at).call
     mapped_provider = Provider.joins(:sources).find_by("provider_sources.source_type": "eic",
                                                        "provider_sources.eid": @eid)
     if mapped_provider.nil?
-      provider = Provider.new(prov)
-      if provider.save!
-        ProviderSource.create!(provider_id: provider.id, source_type: "eic", eid: @eid)
+      mapped_provider = Provider.new(provider_hash)
+      if mapped_provider.save!
+        provider_source = ProviderSource.create!(provider_id: mapped_provider.id, source_type: "eic", eid: @eid)
         Importers::Logo.new(mapped_provider, @eic_provider["logo"]).call
       end
-      provider
     else
-      mapped_provider.update(prov)
+      mapped_provider.update(provider_hash)
+      provider_source = mapped_provider.sources.find_by(source_type: "eic")
       Importers::Logo.new(mapped_provider, @eic_provider["logo"]).call
-      mapped_provider
     end
+    if provider_source.present?
+      mapped_provider.update(upstream_id: provider_source.id)
+    end
+    mapped_provider
   end
 end
--- lib/import/providers.rb
+++ lib/import/providers.rb
@@ -8,6 +8,7 @@ class Import::Providers
                  filepath: nil,
                  unirest: Unirest,
                  logger: ->(msg) { puts msg },
+                 ids: [],
                  default_upstream: :mp,
                  token: nil)
     @eic_base_url = eic_base_url
@@ -15,6 +16,7 @@ class Import::Providers
     @unirest = unirest
     @default_upstream = default_upstream
     @token = token
+    @ids = ids
 
     @logger = logger
     @filepath = filepath
@@ -31,29 +33,56 @@ class Import::Providers
 
     @providers = rp.body["results"].index_by { |provider| provider["id"] }
 
+    updated = 0
+    created = 0
+    not_modified = 0
+    total_provider_count = @providers.length
     output = []
 
-
-    @providers.each do |eid, provider_data|
+    @providers.select { |_p, bodu| @ids.empty? || @ids.include?(_p) }.each do |eid, provider_data|
       output.append(provider_data)
       image_url = provider_data["logo"]
       updated_provider_data = Importers::Provider.new(provider_data, Time.now.to_i, "rest").call
 
       mapped_provider = Provider.joins(:sources).find_by("provider_sources.source_type": "eic",
         "provider_sources.eid": eid) || Provider.find_by(name: updated_provider_data[:name])
+
+      provider_source = ProviderSource.find_by(source_type: "eic", eid: eid)
+
       if mapped_provider.blank?
+        created += 1
         log "Adding [NEW] provider: #{updated_provider_data[:name]}, eid: #{updated_provider_data[:pid]}"
-        mapped_provider = Provider.create!(updated_provider_data)
-        Importers::Logo.new(mapped_provider, image_url).call
-        ProviderSource.create!(provider_id: mapped_provider.id, source_type: "eic", eid: eid)
-      else
+        unless @dry_run
+          mapped_provider = Provider.create!(updated_provider_data)
+          Importers::Logo.new(mapped_provider, image_url).call
+          provider_source = ProviderSource.create!(provider_id: mapped_provider.id, source_type: "eic", eid: eid)
+        end
+      elsif provider_source.present? && provider_source.id == mapped_provider.upstream_id
+        updated += 1
         log "Updating [EXISTING] provider: #{updated_provider_data[:name]}, eid: #{updated_provider_data[:pid]}"
-        mapped_provider.update!(updated_provider_data)
-        Importers::Logo.new(mapped_provider, image_url).call
+        if mapped_provider.upstream_id == provider_source.id
+          unless @dry_run
+            mapped_provider.update!(updated_provider_data)
+            Importers::Logo.new(mapped_provider, image_url).call
+            if mapped_provider.sources.blank?
+              provider_source = ProviderSource.create!(provider_id: mapped_provider.id, source_type: "eic", eid: eid)
+            end
+          end
+        end
+      else
+        not_modified += 1
+        log "Provider upstream is not set to EIC, not updating #{mapped_provider.name}, id: #{mapped_provider.pid}"
       end
-    rescue ActiveRecord::RecordInvalid => e
-      log "[WARN] Provider #{updated_provider_data[:name]} #{updated_provider_data[:pid]} cannot be created. #{e}"
+      if @default_upstream == :eic && provider_source.present?
+        mapped_provider.update(upstream_id: provider_source.id)
+      end
+      rescue ActiveRecord::RecordInvalid => e
+        log "[WARN] Provider #{updated_provider_data[:name]} #{updated_provider_data[:pid]} cannot be created. #{e}"
     end
+
+    Provider.reindex
+
+    log "PROCESSED: #{total_provider_count}, CREATED: #{created}, UPDATED: #{updated}, NOT MODIFIED: #{not_modified}"
 
     unless @filepath.nil?
       open(@filepath, "w") do |file|
