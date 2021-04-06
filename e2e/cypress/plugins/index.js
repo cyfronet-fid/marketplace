3433e17a7c58725dda61edef595842e6da1b7d88
Daniel Kryska <daniel.kryska94@gmail.com>
2021-04-06 12:16:28 +0200

    [#1885] Basic cypress setup - app quality improvement
    - Developer can use profiles depend on a server instance
    - Developer can create typescript commands
    - Developer will see touched elements on site: have a pink border
    - On failure, print all logs in terminal: including XHR body/response
    - Run e2e in Pull Request Github actions
      - Cache npm packages

---
+++ e2e/cypress/plugins/index.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+module.exports = (on, config) => {
+    /**
+     * Show full console log on test fail
+     */
+    require('cypress-terminal-report/src/installLogsPrinter')(on, {printLogsToConsole: 'always'});
+
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        /**
+         * Chrome memory & security optimizations
+         */
+        if (browser.name === 'chrome') {
+            /**
+             * Disable same origin policy
+             */
+            launchOptions.args.push('--disable-web-security');
+
+            /**
+             * Enable tracing bugs inside iframes
+             */
+            launchOptions.args.push('--disable-site-isolation-trials');
+
+            /**
+             * Increase chrome tab memory limit
+             * By default 512MB on 32-bit systems or 1.4GB on 64-bit systems
+             */
+            launchOptions.args.push('--max_old_space_size=1024');
+
+            /**
+             * Increase chrome shared memory space from 64MB to unlimited (since Chrome 65)
+             */
+            launchOptions.args.push('--disable-dev-shm-usage');
+            return launchOptions;
+        }
+    });
+};
+
