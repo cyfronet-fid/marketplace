fb20142f106dcc72a7ed4815332a6d2edb9e3924
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2021-04-07 12:30:11 +0200

    [#1963] Implement events endpoint
    - Implement events endpoint
    - Add events endpoint and serializer specs

--- app/controllers/api/v1/oms/events_controller.rb
+++ app/controllers/api/v1/oms/events_controller.rb
@@ -1,39 +1,32 @@
 # frozen_string_literal: true
 
 class Api::V1::Oms::EventsController < Api::V1::Oms::ApiController
+  before_action :handle_timestamp
+  before_action :handle_limit
+
   def index
-    # TODO: implement endpoint functionality
-    render json: {
-      "events": [
-        {
-          "timestamp": "<timestamp>",
-          "type": "create",
-          "resource": "project",
-          "project_id": 1
-        },
-        {
-          "timestamp": "<timestamp>",
-          "type": "create",
-          "resource": "project_item",
-          "project_id": 1,
-          "project_item_id": 1
-        },
-        {
-          "timestamp": "<timestamp>",
-          "type": "update",
-          "resource": "message",
-          "message_id": 1,
-          "project_id": 1,
-          "project_item_id": 1,
-          "changes": [
-            {
-              "field": "some.path",
-              "before": "value_before",
-              "after": "value_after"
-            }
-          ]
-        }
-      ]
-    }
+    load_events
+    render json: { events: @events.map { |e| OrderingApi::V1::EventSerializer.new(e) } }
+  end
+
+
+  private
+    def load_events
+      @events = policy_scope(@oms.associated_events).limit(@limit).where("created_at > ?", @from_timestamp).order(:created_at)
+    end
+
+    def handle_timestamp
+      params.require(:from_timestamp)
+      @from_timestamp = params[:from_timestamp].to_datetime
+    rescue ActionController::ParameterMissing, ArgumentError => e
+      render json: { error: e }, status: 400
+    end
+
+    def handle_limit
+      @limit = params[:limit].present? ? params[:limit].to_i : 20
+
+      if @limit <= 0
+        render json: { error: "limit must be a positive integer" }, status: 400
+      end
   end
 end
