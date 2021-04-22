aa9cf5a6c6b0ff957bde6853ea4076f1151c9bff
Krzysztof Marszałek <marszalekk@gmail.com>
2021-04-22 14:33:20 +0200

    [#2020] Fix Resource Organisation field requirement
    - field requirement in Resource creation view
    - migration for resource_organisation_id

---
+++ db/migrate/20210422132539_change_resource_organisation_not_null.rb
@@ -0,0 +1,9 @@
+class ChangeResourceOrganisationNotNull < ActiveRecord::Migration[6.0]
+  def up
+    change_column_null(:services, :resource_organisation_id, false)
+  end
+
+  def down
+    change_column_null(:services, :resource_organisation_id, true)
+  end
+end
