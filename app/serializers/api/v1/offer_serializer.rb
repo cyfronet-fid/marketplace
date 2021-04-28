83bb3a4c33baae0e475532e3269b6fc92eaa42f8
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2021-04-28 11:09:47 +0200

    [#1988][#2012] Offers API adjustments
    - Added `internal` and `order_type` dependent serialization
    - Added before_validation hooks to Offer model to ensure `internal`, `primary_oms`, `oms_params` are set accordingly
    - Added migration that sets `internal` to false when `order_type != order_required`

--- app/serializers/api/v1/offer_serializer.rb
+++ app/serializers/api/v1/offer_serializer.rb
@@ -2,9 +2,11 @@
 
 class Api::V1::OfferSerializer < ActiveModel::Serializer
   attribute :iid, key: :id
-  attributes :name, :description, :parameters, :order_type, :webpage, :internal, :order_url
-  attribute :primary_oms_id
-  attribute :oms_params, if: -> { object.oms_params.present? } # TODO: https://github.com/cyfronet-fid/marketplace/issues/1964
+  attributes :name, :description, :parameters, :order_type, :webpage, :order_url
+  attribute  :internal, if: -> { object.order_required? }
+  attribute :primary_oms_id, if: -> { object.internal? }
+  # TODO: https://github.com/cyfronet-fid/marketplace/issues/1964
+  attribute :oms_params, if: -> { object.internal? && object.oms_params.present? }
 
   def primary_oms_id
     object.current_oms.id
