4482860a162894bbfd216bcba31551e712d6c81d
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-05-18 12:10:57 +0200

    [#2012] Remove service.order_target
    Migrate the values of service.order_target to existing
    `order_required && internal` offers' oms_params and remove the column.
    
    In legacy Jira integration, get the SO-ServiceOrderTarget value from the
    offer. Make sure that in case no such value is found an empty string is
    emitted, matching the existing solution.
    
    Remove the order_target logic from add_sombo rake task, and make sure
    that the task handles case when SOMBO OMS already exists, but
    sombo_admin doesn't (see #2079).

---
+++ db/migrate/20210518101302_remove_service_order_target.rb
@@ -0,0 +1,16 @@
+class RemoveServiceOrderTarget < ActiveRecord::Migration[6.0]
+  def change
+    exec_update "
+      UPDATE offers
+      SET
+          oms_params = ('{\"order_target\": \"' || services.order_target || '\"}')::jsonb
+      FROM services
+      WHERE
+            offers.service_id = services.id AND
+            services.order_target <> '' AND
+            offers.order_type = 'order_required' AND
+            offers.internal = true;
+    "
+    remove_column :services, :order_target
+  end
+end
