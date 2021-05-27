806464d5f7459c785d892648f40c699d32c84289
Marcelina <marcelinasiwecka@gmail.com>
2021-05-27 22:30:31 +0200

    Add e2e-tests for home-page
    Add test for providers, dedicated for and platforms list.
    Add test for scientific domain and categories boxes.

---
+++ test/system/cypress/integration/regression_tests/home-page/communities_infrastructures.spec.ts
@@ -0,0 +1,24 @@
+describe("Communities and infrastructures", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  
+  it("should go to service page with selected filter", () => {
+    cy.get("a[href*='related_platforms'][data-e2e='communities_target-user']")
+      .eq(0)
+      .click();
+    cy.location("href")
+      .should("include", "/services?related_platforms");
+    cy.get("[data-e2e='filter-tag']")
+      .should("be.visible");
+  });
+  it("should go to page with all communities/infrastructures", () => {
+    cy.get("a[href*='/communities'][data-e2e='more-link-communities_target-users']")
+      .click();
+    cy.location("href")
+      .should("eq", Cypress.config().baseUrl + "/communities");
+    cy.get("[data-e2e='communities-list']")
+      .contains("Communities and infrastructures")
+      .should("be.visible");
+  });
+});
