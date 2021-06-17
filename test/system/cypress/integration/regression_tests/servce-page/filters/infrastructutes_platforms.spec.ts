c4eebafb663ab42f9698364ac9e66588663f547c
Marcelina <marcelinasiwecka@gmail.com>
2021-06-17 19:27:17 +0200

    Add e2e tests for filters on services page
    Add test for scientific domains, providers, dedicated for,
    platforms, order type, rating, resource availability filters

---
+++ test/system/cypress/integration/regression_tests/servce-page/filters/infrastructutes_platforms.spec.ts
@@ -0,0 +1,17 @@
+describe("Filter related infrastructures and platforms", () => {
+  beforeEach(() => {
+    cy.visit("/services");
+  });
+  
+  it("should select filter", () => {
+    cy.get("[data-e2e='filter-tag']")
+      .should("not.exist")
+    cy.get("#collapse_related_platforms [data-e2e='filter-checkbox']")
+      .eq(0)
+      .click();
+    cy.location("href")
+      .should("include", "/services?related_platforms");
+    cy.get("[data-e2e='filter-tag']")
+      .should("be.visible");
+  });
+});
---
+++ test/system/cypress/integration/regression_tests/servce-page/filters/providers.spec.ts
@@ -0,0 +1,18 @@
+describe("Filter providers", () => {
+  beforeEach(() => {
+    cy.visit("/services");
+  });
+  
+  it("should select filter", () => {
+    cy.get("[data-e2e='filter-tag']")
+      .should("not.exist")
+    cy.get("#collapse_providers [data-e2e='filter-checkbox']")
+      .eq(0)
+      .click();
+    cy.location("href")
+      .should("include", "/services?providers");
+    cy.get("[data-e2e='filter-tag']")
+      .should("be.visible");
+  });
+});
+
