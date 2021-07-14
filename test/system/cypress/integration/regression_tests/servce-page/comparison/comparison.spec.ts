5529bf8ce973dc5d7f81957ed032696e75cb3bfa
Marcelina <marcelinasiwecka@gmail.com>
2021-07-14 20:45:03 +0200

    Add e2e tests for comparison
    Add tests for adding and removing services from comparison.
    Hide cookies policy box and tour guide.

---
+++ test/system/cypress/integration/regression_tests/servce-page/comparison/comparison.spec.ts
@@ -0,0 +1,119 @@
+describe("Comparison", () => {
+  beforeEach(() => {
+    cy.visit("/services");
+    cy.intercept("POST", "/comparisons/services").as("comparison");
+    cy.intercept("GET", "/comparisons").as("comparisonTable");
+  });
+
+  it("should add and remove services from comparison - services from the catalogue page", () => {
+    cy.get("[data-e2e='comparison-checkbox']")
+      .eq(0)
+      .click()
+    cy.wait("@comparison");
+    cy.get("[data-e2e='comparison-bar']")
+      .should("be.visible");
+
+    cy.get("[data-e2e='comparison-checkbox']")
+      .eq(1)
+      .click();
+    cy.wait("@comparison");
+    cy.get("[data-e2e='comparison-checkbox']")
+      .eq(2)
+      .click();
+    cy.wait("@comparison");
+    cy.get("[data-e2e='comparison-checkbox']")
+      .eq(3)
+      .should("have.attr", "disabled");
+
+    cy.get("[data-e2e='compare-btn']")
+      .click();
+    cy.wait("@comparisonTable");
+    cy.location("href")
+      .should("include", "/comparisons");
+    
+    cy.get("[data-e2e='services-comparison-table']")
+      .should("be.visible")
+    cy.get("[data-e2e='delete-service-btn'] svg")
+      .should("have.length", 3)
+      .eq(0)
+      .click()
+   
+    cy.wait("@comparisonTable");
+    cy.get("[data-e2e='add-next-resource-btn']")
+      .click();
+    cy.location("href")
+      .should("include", "/services");
+
+    
+    cy.get("[data-e2e='comparison-clearAll-btn']")
+      .click();
+    cy.get("[data-e2e='comparison-bar']")
+      .should("not.be.visible");
+  });
+
+  it("should add and remove services from comparison - services from details page", () => {
+    cy.get("[data-e2e='service-name']")
+      .eq(0)
+      .click();
+    cy.get("[data-e2e='access-resource-btn']")
+      .should("be.visible");
+
+    cy.get("[data-e2e='comparison-checkbox']")
+      .click()
+    cy.get("[data-e2e='comparison-bar']")
+      .should("be.visible");
+
+    cy.go("back");
+
+    cy.get("[data-e2e='service-name']")
+      .eq(2)
+      .click();
+    cy.get("[data-e2e='access-resource-btn']")
+      .should("be.visible");
+    cy.get("[data-e2e='comparison-checkbox']")
+      .click();
+
+    cy.get("[data-e2e='compare-btn']")
+      .click();
+    cy.wait("@comparisonTable");
+    cy.location("href")
+      .should("include", "/comparisons");
+    cy.get("[data-e2e='add-next-resource-btn']")
+      .click();
+    cy.location("href")
+      .should("include", "/services");
+
+    cy.get("[data-e2e='delete-service-btn']")
+      .should("have.length", 2)
+      .eq(0)
+      .click();
+    cy.get("[data-e2e='delete-service-btn']")
+      .should("have.length", 1)
+      .eq(0)
+      .click();
+    cy.get("[data-e2e='comparison-bar']")
+      .should("not.be.visible");
+  });
+
+  it("should add and remove services from comparison - toggle comparison via checkbox", ()=>{
+    cy.get("[data-e2e='comparison-checkbox']")
+      .next()
+      .contains('Add to comparison')
+      .eq(0)
+      .click()
+      .contains('Remove from comparison')
+    cy.wait("@comparison");
+    cy.get("[data-e2e='comparison-bar']")
+      .should("be.visible");
+
+    cy.get("[data-e2e='comparison-checkbox']")
+      .next()
+      .contains('Remove from comparison')
+      .eq(0)
+      .click()
+      .contains('Add to comparison')
+    cy.wait("@comparison");
+    cy.get("[data-e2e='comparison-bar']")
+      .should("not.be.visible");
+  })
+});
