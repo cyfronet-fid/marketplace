627b70a208366664fd9485cd8a226dd500b3b0e0
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-07-20 09:50:21 +0200

    [#2172] Extend API docs
    Add more detailed API docs.

--- app/helpers/api_docs_helper.rb
+++ app/helpers/api_docs_helper.rb
@@ -2,6 +2,14 @@
 
 module ApiDocsHelper
   def api_wiki_subsections
-    [:introduction, :authentication]
+    %i[introduction basic_information integration_methods oms_configuration access_model project_item_workflows]
+  end
+
+  def humanized_subsection(subsection)
+    subsection.to_s.humanize
+  end
+
+  def next_subsection(current_subsection)
+    api_wiki_subsections[api_wiki_subsections.index(current_subsection) + 1]
   end
 end
