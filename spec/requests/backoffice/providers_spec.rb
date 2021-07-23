672b9d09f3274e824599c1791fa352b5e8d13f08
danielkryska <daniel.kryska94@gmail.com>
2021-07-23 11:10:56 +0200

    [#2203] Fix the provider backoffice form
    - trim the form values before validation
    - permit the attributes on provider update from params
    instead of the current record

--- spec/requests/backoffice/providers_spec.rb
+++ spec/requests/backoffice/providers_spec.rb
@@ -14,5 +14,18 @@ RSpec.describe "Backoffice: manage providers" do
       expect { delete backoffice_provider_path(category) }.
         to change { Provider.count }.by(-1)
     end
+
+    it "should call permitted_attributes with provider with form upstream_id" do
+      provider = create(:provider)
+      new_params = {
+        name: "test1111111",
+        abbreviation: "test 111111"
+      }
+      put backoffice_provider_path(provider), params: { provider: { upstream_id: nil, **new_params } }
+
+      provider.reload
+      expect(provider.upstream_id).to eq(nil)
+      new_params.each { |key, value| expect(provider[key]).to eq(value) }
+    end
   end
 end
