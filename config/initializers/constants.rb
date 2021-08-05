e5653ccdf6e3fefbeac13989e3ca41a288108254
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-08-05 14:59:58 +0200

    [#2063] Read version from VERSION file
    Before, only ENV["MP_VERSION"] was accessed for the version value,
    requiring the deployer to set the constant on each run.
    
    Now, the constant still takes precedence if set, but by default the
    VERSION file's content is used, making it less error-prone, but allowing
    for flexibility in case other behaviour is desired.
    
    In the view, it's no longer necessary to check for the constant's
    existence, since if the VERSION file doesn't exist then error is raised
    during app initialization.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+MP_VERSION = ENV["MP_VERSION"] || File.read(Rails.root.join("VERSION")).strip
+
 # time after which user can rate service that starts when service become ready
 # default value is set to 90 days, ENV variable should represent number of days
 RATE_AFTER_PERIOD = ENV["RATE_AFTER_PERIOD"].present? ? (ENV["RATE_AFTER_PERIOD"]).to_i.days : 90.days
