85ad12876ef35c5be51536f39698a6958463942c
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-08-05 14:59:58 +0200

    [#2063] Read version from VERSION file
    Before, only ENV["MP_VERSION"] was accessed for the version value,
    requiring the deployer to set the constant on each run.
    
    Now, the constant still takes precedence if set, but by default the
    VERSION file's content is used, making it less error-prone, but allowing
    for flexibility in case other behaviour is desired.
    
    In the view, it's no longer necessary to check for the constant's
    existence, since if the VERSION file doesn't exist then error is raised
    during app initialization.

--- spec/features/admin/version_spec.rb
+++ spec/features/admin/version_spec.rb
@@ -6,15 +6,21 @@ RSpec.feature "Marketplace version" do
   include OmniauthHelper
 
   let(:admin) { create(:user, roles: [:admin]) }
+  let(:version) { File.read(Rails.root.join("VERSION")).strip }
 
   before { checkin_sign_in_as(admin) }
 
-  scenario "can been seen in admin main page" do
-    allow(ENV).to receive(:[]).and_call_original
-    allow(ENV).to receive(:[]).with("MP_VERSION").and_return("1234")
+  scenario "is visible in admin main page" do
+    visit admin_path
+
+    expect(page).to have_content(version)
+  end
+
+  scenario "is read from MP_VERSION constant" do
+    stub_const("MP_VERSION", "foo:bar")
 
     visit admin_path
 
-    expect(page).to have_content("1234")
+    expect(page).to have_content("foo:bar")
   end
 end
