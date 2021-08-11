31db7abfbe5458f32d78005f3c05ca7c51c5611d
danielkryska <daniel.kryska94@gmail.com>
2021-08-11 12:56:06 +0200

    [#2003] Fix new resource preview error

--- app/models/concerns/logo_attachable.rb
+++ app/models/concerns/logo_attachable.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module LogoAttachable
+  include ImageHelper
+
   def logo_variable
     if logo.present? && !logo.variable?
-      errors.add(:logo, "^The logo format you're trying to attach is not supported.
-                          Supported formats: png, gif, jpg, jpeg,
-                          pjpeg, tiff, vnd.adobe.photoshop or vnd.microsoft.icon.")
+      errors.add(:logo, ImageHelper.permitted_ext_message)
     end
   end
 end
