33fbceef439ffbd8005d4080dc772a1e97def81d
Marcelina <marcelinasiwecka@gmail.com>
2021-08-11 11:49:27 +0200

    Add e2e tests for my profile
    Add tests for adding and removing additional information
    and email notification in my profile.
    Hide cookies policy box and tour guide in my profile.

---
+++ test/system/cypress/integration/regression_tests/servce-page/my_eosc_marketplace/my-profile.spec.ts
@@ -0,0 +1,89 @@
+import { UserFactory } from "../../../../factories/user.factory";
+describe("My profile", () => {
+  const user = UserFactory.create();
+
+  beforeEach(() => {
+    cy.visit("/services");
+    cy.loginAs(user);
+    cy.get("[data-e2e='my-eosc-button']")
+      .click();
+    cy.get("[data-e2e='profile']")
+      .click();
+    cy.location("href")
+      .should("contain", "/profile");
+  });
+
+  it("should add and remove Additional information", () => {
+    cy.get("[data-e2e='additional-inf-edit']")
+      .click();
+    cy.get("[data-e2e='categories-select']")
+      .parent()
+      .click()
+      .then(($el) => {
+        cy.wrap($el).next().find(".choices__item").eq(1).click();
+        cy.wrap($el).next().find(".choices__item").eq(2).click();
+      });
+    cy.get("body").type("{esc}");
+    cy.get("[data-e2e='scientific-domain-select']")
+      .parent()
+      .click()
+      .then(($el) => {
+        cy.wrap($el).next().find(".choices__item").eq(1).click();
+      });
+    cy.get("body").type("{esc}");
+    cy.get("[data-e2e='submit-btn']")
+      .click();
+    cy.get("[data-e2e='category-item'] li")
+      .should("have.length", 2);
+    cy.get("[data-e2e='scientific-domain-item'] li")
+      .should("have.length", 1);
+
+    cy.get("[data-e2e='additional-inf-edit']")
+      .click();
+    cy.get(".user_scientific_domains .choices__button")
+      .contains("Remove item")
+      .click();
+    cy.get("[data-e2e='submit-btn']")
+      .click();
+    cy.get("[data-e2e='scientific-domain-item'] li")
+      .should("have.length", 0);
+
+    cy.get("[data-e2e='additional-inf-edit']")
+      .click();
+    cy.get("[data-e2e='delete-btn']")
+      .click();
+    cy.get("[data-e2e='category-item'] li")
+      .should("have.length", 0);
+    cy.get("[data-e2e='scientific-domain-item'] li")
+      .should("have.length", 0);
+  });
+
+  it("should add and remove Email notifications", () => {
+    cy.get("[data-e2e='email-notif-edit']")
+      .click();
+    cy.get("#user_categories_updates")
+      .click();
+    cy.get("#user_scientific_domains_updates")
+      .click();
+    cy.get("[data-e2e='submit-btn']")
+      .click();
+    cy.get("[data-e2e='email-notif-item'] li")
+      .should("have.length", 2);
+
+    cy.get("[data-e2e='email-notif-edit']")
+      .click();
+    cy.get("#user_categories_updates")
+      .click();
+    cy.get("[data-e2e='submit-btn']")
+      .click();
+    cy.get("[data-e2e='email-notif-item'] li")
+      .should("have.length", 1);
+
+    cy.get("[data-e2e='email-notif-edit']")
+      .click();
+    cy.get("[data-e2e='delete-btn']")
+      .click();
+    cy.get("[data-e2e='email-notif-item'] li")
+      .should("have.length", 0);
+  });
+});
