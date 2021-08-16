d86624596361b79cb23bfe9cc7c2c0f42a9b9856
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2021-08-16 16:28:49 +0200

    [#2083] Fix choices field in modals

--- app/javascript/app/choices.js
+++ app/javascript/app/choices.js
@@ -1,7 +1,10 @@
 import Choices from 'choices.js'
 
 export default function initChoices(scope = document) {
-  const elements = scope.querySelectorAll('[data-choice]');
+  // The "=true" portion ensures that the function will apply only to previously unhandled elements,
+  // because the Choices lib will change the "data-choice" value from "true" to "active" after application.
+  // This makes this function idempotent.
+  const elements = scope.querySelectorAll('[data-choice="true"]');
 
   elements.forEach(function(element) {
     new Choices(element, {
