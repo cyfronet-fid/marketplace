eb0708f886949c9498b9227c66d1ae8358007187
Marcelina <marcelinasiwecka@gmail.com>
2021-08-25 16:15:25 +0200

    Add e2e tests for searchbar
    Add tests for searching items in the searchbar

---
+++ test/system/cypress/integration/regression_tests/servce-page/mainsearch/mainsearch.spec.ts
@@ -0,0 +1,88 @@
+describe("Main Search", () => {
+  beforeEach(() => {
+    cy.visit("/services");
+  });
+
+  it("should display Resource according to the phrase you entered into searchbar", () => {
+    cy.get("[data-e2e='searchbar-input']")
+      .type("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("Resources")
+      .next()
+      .click();
+    cy.url().should("contain", "EGI");
+    cy.get("[data-e2e='access-resource-btn']")
+      .should("be.visible");
+  });
+
+  it("should display Offer according to the phrase you entered into searchbar", () => {
+    cy.get("[data-e2e='searchbar-input']")
+      .type("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("Offer")
+      .next()
+      .click();
+    cy.url().should("contain", "EGI")
+      .and("contain", "#offer");
+    cy.get("[data-e2e='access-resource-btn']")
+      .should("be.visible");
+  });
+
+  it("should display Provider according to the phrase you entered into searchbar", () => {
+    cy.get("[data-e2e='searchbar-input']")
+      .type("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("EGI");
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("Provider")
+      .next()
+      .click();
+    cy.url()
+      .should("contain", "EGI")
+      .and("contain", "provider");
+    cy.get("[data-e2e='btn-browse-resource']")
+      .should("be.visible");
+  });
+
+  it("should display a list of results containing typed phrase", () => {
+    cy.url()
+      .should("not.include", "sort=")
+      .and("not.include","q=")
+    cy.get("[data-e2e='searchbar-input']")
+      .type("EGI");
+    cy.get("[data-e2e='query-submit-btn']")
+      .click();
+    cy.url()
+      .should("include", "sort=")
+      .and("include","q=");
+    cy.get('#sort.query-sort [selected="selected"]')
+      .should("have.text", "Best match");
+    cy.get("[data-e2e='service-name']")
+      .should("include.text", "EGI");
+    cy.get("[data-e2e='search-clear-btn']")
+      .click();
+    cy.get("[data-e2e='searchbar-input']")
+      .should("not.have.text");
+    cy.url()
+      .should("not.include", "sort")
+      .and("not.include","q=");
+  });
+
+  it("should selected category", ()=>{
+    cy.get("[data-e2e='category-select'] > option")
+    .eq(1)
+    .invoke("text")
+    .then(value=>{
+      cy.get("[data-e2e='category-select']")
+        .select(value)
+    })
+    cy.get("[data-e2e='query-submit-btn']")
+      .click();
+    cy.location("href")
+      .should("include", "services/c/");
+  })
+});
