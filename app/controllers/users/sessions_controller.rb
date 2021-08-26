e6b02bdc2d9a186a5923b577839e6587a1d39bb1
danielkryska <daniel.kryska94@gmail.com>
2021-08-26 08:13:48 +0200

    [#2190] Remove autologin POC

--- app/controllers/users/sessions_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Users::SessionsController < Devise::SessionsController
-  def destroy
-    super
-    cookies.delete(:internal_session)
-    cookies.delete(:eosc_logged_in, domain: Mp::Application.config.autologin_domain)
-  end
-end
