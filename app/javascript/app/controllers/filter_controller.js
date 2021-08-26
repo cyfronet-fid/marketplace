bfa23300cd8f00d97cc559a3978d01d0ee2ae436
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2021-08-26 16:59:05 +0200

    Add js handler for selected values
    Check filter values by filter_controller
    and set values from query params
    Needed for automatic uncheck by using
    back arrow in the browser

--- app/javascript/app/controllers/filter_controller.js
+++ app/javascript/app/controllers/filter_controller.js
@@ -3,6 +3,11 @@ import {Controller} from 'stimulus'
 export default class extends Controller {
   static targets = ['form']
 
+  pageShow() {
+    this.checkActiveMulticheckboxFilters();
+    this.checkActiveSelectFilters();
+  }
+
   toggle(event) {
     const target = event.currentTarget;
     const collapsed = !target.classList.contains("collapsed");
@@ -20,6 +25,35 @@ export default class extends Controller {
     }
     form.submit();
     document.getElementsByClassName("spinner-background")[0].style.display = 'flex';
+  }
+
+  checkActiveMulticheckboxFilters() {
+    let checkboxes = this.formTarget.getElementsByTagName("input");
+    const urlSearchParams = new URLSearchParams(window.location.search);
+
+    for(const element of checkboxes) {
+      const params = urlSearchParams.getAll(element.name);
+        if(!(params === element.value || params.includes(element.value)) && element.type == "checkbox" ) {
+          element.checked = false;
+        } else if (element.type == "checkbox") {
+          element.checked = true;
+        }
+    }
+  }
+
+  checkActiveSelectFilters() {
+    let selectable = this.formTarget.getElementsByTagName("select");
+
+    const urlSearchParams = new URLSearchParams(window.location.search);
+
+    for(const element of selectable) {
+      const param = urlSearchParams.get(element.name);
+      if(param === element.value) {
+        element.value = param;
+      } else {
+        element.value = "";
+      }
+    }
   }
 
   expireAtString() {
