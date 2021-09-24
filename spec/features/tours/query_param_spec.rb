7738ee32000409d2fed1bd8ae810a02edcb3d626
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-09-24 12:18:39 +0200

    [#2316] Clear query param on tour complete
    The `tour` query param wasn't cleared on tour completion for tours with
    query param activation strategy.

--- spec/features/tours/query_param_spec.rb
+++ spec/features/tours/query_param_spec.rb
@@ -67,6 +67,8 @@ RSpec.feature "Query param tour" do
     click_on "Next"
     click_on "Next"
 
+    expect(page).to have_current_path(service_details_path(service))
+
     expect(page).to have_text("Congratulations!")
     expect(page).to have_text("You have completed the resource presentation page tour guide.")
     expect(page).to have_text("Please also leave a comment on how we can improve it")
