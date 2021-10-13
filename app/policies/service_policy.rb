9f89832675f665f83b3f84f1d059800e107a72d8
Krzysztof Marszałek <marszalekk@gmail.com>
2021-10-13 11:20:38 +0200

    [#2276] Fix Offers step for Resource ordering
    - change view condition to policy methods
    - narrow offers to published and concat bundles and offers
    - bundled offers are always shown

--- app/policies/service_policy.rb
+++ app/policies/service_policy.rb
@@ -12,14 +12,27 @@ class ServicePolicy < ApplicationPolicy
   end
 
   def offers_show?
-    record.offers? && record.offers.select { |s| s.bundled_offers_count == 0 && s.published? }.size > 1
+    enough_to_show? && has_offers?
   end
 
   def bundles_show?
-    record.offers? && record.offers.any? { |s| s.bundled_offers_count > 0 }
+    has_bundled_offers?
   end
 
   def data_administrator?
     record.administered_by?(user)
   end
+
+  private
+    def enough_to_show?
+      record.offers? && record.offers.published.size > 1
+    end
+
+    def has_bundled_offers?
+      record.offers? && record.offers.any? { |s| s.bundled_offers_count > 0 && s.published? }
+    end
+
+    def has_offers?
+      record.offers? && record.offers.any? { |s| s.bundled_offers_count == 0 && s.published? }
+    end
 end
