5819993c4feaa6b5882fcf7f543c8e42842bea10
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-10-15 15:42:31 +0200

    Enqueue async jobs after bundle transaction is committed
    Move the transactional logic to the ProjectItem::Create service and only
    enqueue the asynchronous jobs when the transaction has been committed,
    so that the job runner doesn't try to execute the jobs before the
    entities have been created.

--- app/services/project_item/create.rb
+++ app/services/project_item/create.rb
@@ -1,19 +1,48 @@
 # frozen_string_literal: true
 
 class ProjectItem::Create
-  def initialize(project_item, message = nil)
+  def initialize(project_item, message = nil, bundle_params: nil)
     @project_item = project_item
     @message = message
+    @bundle_params = bundle_params.respond_to?(:[]) ? bundle_params : {}
   end
 
   def call
-    if @project_item.update(status: "created", status_type: :created)
-      if orderable?
-        ProjectItem::RegisterJob.perform_later(@project_item, @message)
-        ProjectItemMailer.created(@project_item).deliver_later
-      else
-        ProjectItem::ReadyJob.perform_later(@project_item, @message)
-        ProjectItemMailer.added_to_project(@project_item).deliver_later
+    rolled_back = false
+    bundled_project_items = []
+
+    ProjectItem.transaction do
+      if !@project_item.update(status: "created", status_type: :created)
+        rolled_back = true
+        raise ActiveRecord::Rollback
+      end
+
+      if @project_item&.offer.bundle?
+        bundled_project_items = @project_item.offer.bundled_offers.map do |offer|
+          ProjectItem.create(status: "created",
+                             status_type: :created,
+                             parent_id: @project_item.id,
+                             project_id: @project_item.project_id,
+                             offer_id: offer.id,
+                             properties: @bundle_params[offer.id] || [])
+        end
+
+        if bundled_project_items.any? { |pi| !pi.persisted? }
+          rolled_back = true
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+    if !rolled_back
+      ([@project_item] + bundled_project_items).each do |project_item|
+        if orderable?(project_item)
+          ProjectItem::RegisterJob.perform_later(project_item, @message)
+          ProjectItemMailer.created(project_item).deliver_later
+        else
+          ProjectItem::ReadyJob.perform_later(project_item, @message)
+          ProjectItemMailer.added_to_project(project_item).deliver_later
+        end
       end
     end
 
@@ -21,7 +50,7 @@ class ProjectItem::Create
   end
 
   private
-    def orderable?
-      @project_item.offer.orderable?
+    def orderable?(project_item)
+      project_item.offer.orderable?
     end
 end
