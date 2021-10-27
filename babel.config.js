a65d466a20920407adf428fb10822911996e24b8
Jakub Sawicki <j.sawicki@cyfronet.pl>
2021-10-27 14:15:12 +0200

    Nodejs upgrades and workflow improvement
    Update nodejs to v16 and conservatively upgrade js dependencies.
    Webpacker is major-bumped, to be in line with ruby version.
    
    Add yarn dependency caching and use container-provided executable in
    workflows.

--- babel.config.js
+++ babel.config.js
@@ -41,12 +41,7 @@ module.exports = function(api) {
       require('@babel/plugin-syntax-dynamic-import').default,
       isTestEnv && require('babel-plugin-dynamic-import-node'),
       require('@babel/plugin-transform-destructuring').default,
-      [
-        require('@babel/plugin-proposal-class-properties').default,
-        {
-          loose: true
-        }
-      ],
+      require('@babel/plugin-proposal-class-properties').default,
       [
         require('@babel/plugin-proposal-object-rest-spread').default,
         {
