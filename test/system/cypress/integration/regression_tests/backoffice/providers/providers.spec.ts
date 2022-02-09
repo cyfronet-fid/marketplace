3302ace1a8a635b4aa5fca479df746ee8ab149e2
Marcelina <marcelinasiwecka@gmail.com>
2022-02-09 08:09:06 +0100

    Add e2e tests for draft and created by fully form providers
    Add test for create provider where all the fields are filled,
    Add test for checking visibility draft provider in MP na BO

--- test/system/cypress/integration/regression_tests/backoffice/providers/providers.spec.ts
+++ test/system/cypress/integration/regression_tests/backoffice/providers/providers.spec.ts
@@ -1,4 +1,4 @@
-import { ProvidersFactory } from "../../../../factories/provider.factory";
+import { ProvidersFactory, ProvidersFactoryExtended } from "../../../../factories/provider.factory";
 import { UserFactory } from "../../../../factories/user.factory";
 
 describe("Providers", () => {
@@ -7,15 +7,29 @@ describe("Providers", () => {
     ProvidersFactory.create()
   );
 
+  const [providerExtented] = [...Array(1)].map(() =>  
+  ProvidersFactoryExtended.create()
+  );
+
   const correctLogo = "logo.jpg";
   const wrongLogo = "logo.svg";
 
+  const providerStatusDraft = "LifeWatch ERIC";
   const providerWithResourceDeleted = "D4Science Infrastructure";
   const providerWithResourceDraft = "European Space Agency (ESA)";
   const providerWithResourcePublished = "EUDAT";
   const providerWithResourceErrored = "CSC";
   const providerWithResourceUnverified = "Institute of Atmospheric Pollution - National Research Council of Italy (CNR-IIA)"
   const resourceProviderForPublishedResource = "Interuniversity consortium CIRMMP";
+
+  const successCreationMessage = "New provider created successfully";
+  const successDeletionMessage = "Provider removed successfully";
+  const successUpdationMessage = "Provider updated successfully";
+  const alertDeletionMessage = "This Provider has resources connected to it, therefore is not possible to remove it.";
+  const alertLogoValidation = "Logo is not a valid file format and Logo format you're trying to attach is not supported. " +
+  "Supported formats: png, gif, jpg, jpeg, pjpeg, tiff, vnd.adobe.photoshop or vnd.microsoft.icon";
+  const alertUrlValidation = "Website isn't valid or website doesn't exist, please check URL";
+  const alertEmailValidation = "Email is not a valid email address";
 
   beforeEach(() => {
     cy.visit("/");
@@ -52,8 +66,7 @@ describe("Providers", () => {
     cy.get("[data-e2e='create-provider-btn']")
       .click();
     cy.contains(
-      "div.alert-success",
-      "New provider created successfully")
+      "div.alert-success", successCreationMessage)
       .should("be.visible");
     cy.get("h1")
       .invoke("text")
@@ -108,29 +121,23 @@ describe("Providers", () => {
   });
 
   it("shouldn't add new provider", () => {
-    cy.visit("/backoffice/providers");
-    cy.get("[data-e2e='add-new-provider']")
-      .click();
+    cy.visit("/backoffice/providers/new");
     cy.location("href")
       .should("contain", "/providers/new");
     cy.fillFormCreateProvider({...provider, basicWebpage_url:"wrongFormat", adminEmail:"wrongFormat"}, wrongLogo);
     cy.get("[data-e2e='create-provider-btn']")
       .click();
     cy.contains(
-      "div.invalid-feedback",
-      "Logo is not a valid file format and Logo format you're trying to attach is not supported. " +
-      "Supported formats: png, gif, jpg, jpeg, pjpeg, tiff, vnd.adobe.photoshop or vnd.microsoft.icon")
+      "div.invalid-feedback", alertLogoValidation)
       .should("be.visible");
-    cy.contains("div.invalid-feedback", "Website isn't valid or website doesn't exist, please check URL")
+    cy.contains("div.invalid-feedback", alertUrlValidation )
       .should("be.visible");
-    cy.contains("div.invalid-feedback", "Email is not a valid email address")
+    cy.contains("div.invalid-feedback", alertEmailValidation)
       .should("be.visible");
   });
 
   it("should deleted provider without resource and find only in filter and autocomplete in backoffice", () => {
-    cy.visit("/backoffice/providers");
-    cy.get("[data-e2e='add-new-provider']")
-      .click();
+    cy.visit("/backoffice/providers/new");
     cy.location("href")
       .should("contain", "/providers/new");
     cy.fillFormCreateProvider(provider2, correctLogo);
@@ -141,7 +148,7 @@ describe("Providers", () => {
       .then((value) => {
         cy.contains("a", "Delete")
           .click();
-        cy.contains("div.alert-success", "Provider removed successfully")
+        cy.contains("div.alert-success", successDeletionMessage)
           .should("be.visible");
         cy.visit("/");
         cy.get("a[data-e2e='more-link-providers']")
@@ -187,7 +194,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-success", "Provider removed successfully")
+    cy.contains("div.alert-success", successDeletionMessage)
       .should("be.visible");
   });
 
@@ -197,8 +204,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-danger", 
-      "This Provider has resources connected to it, therefore is not possible to remove it.")
+    cy.contains("div.alert-danger", alertDeletionMessage)
       .should("be.visible");
   });
 
@@ -208,8 +214,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-danger", 
-      "This Provider has resources connected to it, therefore is not possible to remove it.")
+    cy.contains("div.alert-danger", alertDeletionMessage)
       .should("be.visible");
   });
 
@@ -219,8 +224,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-danger", 
-      "This Provider has resources connected to it, therefore is not possible to remove it.")
+    cy.contains("div.alert-danger", alertDeletionMessage)
       .should("be.visible");
   });
 
@@ -230,8 +234,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-danger", 
-      "This Provider has resources connected to it, therefore is not possible to remove it.")
+    cy.contains("div.alert-danger", alertDeletionMessage)
       .should("be.visible");
   });
 
@@ -241,7 +244,7 @@ describe("Providers", () => {
       .parents('li.providers')
       .find("a.delete-icon")
       .click();
-    cy.contains("div.alert-success", "Provider removed successfully")
+    cy.contains("div.alert-success", successDeletionMessage)
       .should("be.visible");
   });
 
@@ -255,10 +258,92 @@ describe("Providers", () => {
     cy.fillFormCreateProvider({basicName: "Edited provider"}, correctLogo);
     cy.get("[data-e2e='create-provider-btn']")
       .click();
-    cy.get(".alert-success")
-      .contains("Provider updated successfully")
+    cy.contains(".alert-success",successUpdationMessage)
       .should("be.visible");
     cy.contains("h1", "Edited provider")
       .should("be.visible");
+  });
+
+  it("should check if draft provider is visible only in Backoffice", () => {
+    cy.get("a[data-e2e='more-link-providers']")
+      .click();
+    cy.contains("a", providerStatusDraft)
+      .should("not.exist");
+    cy.visit("/services")
+    cy.get("[data-e2e='searchbar-input']")
+      .type(providerStatusDraft);
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .should("not.have.text", providerStatusDraft)
+    cy.get("#collapse_providers [data-e2e='multicheckbox-search']")
+      .type(providerStatusDraft);
+    cy.get("#collapse_providers li")
+      .contains(providerStatusDraft)
+      .should("not.exist");
+
+    cy.visit("/backoffice/services");
+    cy.get("#collapse_providers [data-e2e='multicheckbox-search']")
+      .type(providerStatusDraft);
+    cy.get("[data-e2e='filter-checkbox']")
+      .next()
+      .contains(providerStatusDraft)
+      .parent()
+      .click();
+    cy.get("[data-e2e='filter-tag']")
+      .should("be.visible");
+    cy.get("[data-e2e='service-id'] span")
+      .should("have.text", "draft")
+    cy.get("[data-e2e='searchbar-input']")
+      .type(providerStatusDraft);
+    cy.get("[data-e2e='autocomplete-results'] li")
+      .contains("Provider")
+      .next()
+      .contains(providerStatusDraft)
+      .click();
+    cy.location("href")
+      .should("contain", "/backoffice/providers/");
+    cy.contains("h1", providerStatusDraft)
+      .should("be.visible");
+  });
+
+  it("should go to Backoffice and create provider by filling in all fields", () => {
+    cy.visit("/backoffice/providers");
+    cy.get("[data-e2e='add-new-provider']")
+      .click();
+    cy.location("href")
+      .should("contain", "/providers/new");
+    cy.fillFormCreateProvider(providerExtented, correctLogo);
+    cy.get("[data-e2e='create-provider-btn']")
+      .click();
+    cy.contains(
+      "div.alert-success", successCreationMessage)
+      .should("be.visible");
+    cy.get("h1")
+      .invoke("text")
+      .then((value) => {
+        cy.visit("/");
+        cy.get("a[data-e2e='more-link-providers']")
+          .click();
+        cy.contains("a", value)
+          .click();
+        cy.contains("h2", value)
+          .should("be.visible");
+        cy.get("[data-e2e='provider-details-btn']")
+          .click();
+        cy.location("href")
+          .should("contain", "/details")
+        cy.contains("h5", "Networks")
+          .should("be.visible");
+        cy.intercept("/providers/*").as("providerPage")
+        cy.get("[data-e2e='provider-about-btn']")
+          .click();
+        cy.wait("@providerPage")
+        cy.get("a[data-e2e='tag-btn']")
+          .should("be.visible")
+          .click();
+        cy.location("href")
+          .should("contain", "/services?tag=");
+        cy.get("[data-e2e='filter-tag']")
+          .should("be.visible")
+      });
   });
 });
--- test/system/cypress/support/providers.ts
+++ test/system/cypress/support/providers.ts
@@ -1,164 +1,274 @@
 /**
  * Define new commands types for typescript (for autocompletion)
  */
- import { IProviders } from "../factories/provider.factory";
+import { IProviders, IProvidersExtended } from "../factories/provider.factory";
 
- declare global {
-   namespace Cypress {
-     interface Chainable {
-      
-       fillFormCreateProvider(provider: Partial<IProviders>, logo:any): Cypress.Chainable<void>;
- 
-     }
-   }
- }
- 
- const selectItem = (provider: string[], selector: string) => {
-   cy.get(selector).then(($el) => {
-     $el
-       .find(".choices__item--selectable")
-       .find(".choices__button")
-       .each((index, $btn) => $btn.click());
-   });
- 
-   if (provider && provider.length > 0) {
+declare global {
+  namespace Cypress {
+    interface Chainable {
+
+      fillFormCreateProvider(provider: Partial<IProvidersExtended>, logo: any): Cypress.Chainable<void>;
+
+    }
+  }
+}
+
+const selectItem = (provider: string[], selector: string) => {
+  cy.get(selector).then(($el) => {
+    $el
+      .find(".choices__item--selectable")
+      .find(".choices__button")
+      .each((index, $btn) => $btn.click());
+  });
+
+  if (provider && provider.length > 0) {
     provider.forEach((el) => {
-       cy.get(selector)
-         .find('.choices__input[type="search"]')
-         .type(el)
-         .type("{enter}");
-       cy.get(selector)
-         .find(".choices__item.choices__item--selectable")
-         .contains(el)
-         .should("exist");
-     });
-   }
- };
- 
- Cypress.Commands.add("fillFormCreateProvider", (provider: IProviders, logo) => {
-   cy.get("#basic-header")
-     .click();
- 
-   if (provider.basicName) {
-     cy.get("#provider_name")
-       .clear({ force: true })
-       .type(provider.basicName);
-   }
- 
-   if (provider.basicAbbreviation) {
-     cy.get("#provider_abbreviation")
-       .clear({ force: true })
-       .type(provider.basicName);
-   }
- 
-   if (provider.basicWebpage_url) {
-     cy.get("#provider_website")
-       .clear({ force: true })
-       .type(provider.basicWebpage_url);
-   }
- 
-   cy.get("#marketing-header")
-     .click();
- 
-   if (provider.marketingDescription) {
-     cy.get("#provider_description")
-       .clear()
-       .type(provider.marketingDescription);
-   }
+      cy.get(selector)
+        .find('.choices__input[type="search"]')
+        .type(el)
+        .type("{enter}");
+      cy.get(selector)
+        .find(".choices__item.choices__item--selectable")
+        .contains(el)
+        .should("exist");
+    });
+  }
 
-   cy.get("#provider_logo")
-     .attachFile(logo);
- 
-   if (provider.marketingMultimedia) {
-     cy.get("#provider_multimedia_0")
-       .clear()
-       .type(provider.marketingMultimedia);
-   }
- 
-   cy.get("#classification-header").click();
- 
-   selectItem(provider.classificationScientificDomains,".provider_scientific_domains");
- 
-   cy.get("body")
-     .type("{esc}");
- 
-   if (provider.classificationTag) {
-     cy.get("#provider_tag_list_0")
-       .clear()
-       .type(provider.classificationTag);
-    }
+  cy.get("body")
+  .type("{esc}");
+};
 
-   cy.get("#location-header")
-     .click()
+Cypress.Commands.add("fillFormCreateProvider", (provider: IProvidersExtended, logo) => {
+  cy.get("#basic-header")
+    .click();
 
-     if (provider.locationStreet) {
-      cy.get("#provider_street_name_and_number")
-        .clear({ force: true })
-        .type(provider.locationStreet);
-    }
-  
-    if (provider.locationPostCode) {
-      cy.get("#provider_postal_code")
-        .clear({ force: true })
-        .type(provider.locationPostCode);
-    }
-  
-    if (provider.locationCity) {
-      cy.get("#provider_city")
-        .clear({ force: true })
-        .type(provider.locationCity);
-    }
-  
-    if (provider.locationCountry) {
-      cy.get("#provider_country")
-        .select(provider.locationCountry);
-    }
- 
-   cy.get("#contact-header")
-     .click();
- 
-   if (provider.contactFirstname) {
-     cy.get("#provider_main_contact_attributes_first_name")
-       .clear({ force: true })
-       .type(provider.contactFirstname);
-   }
- 
-   if (provider.contactLastname) {
-     cy.get("#provider_main_contact_attributes_last_name")
-       .clear({ force: true })
-       .type(provider.contactLastname);
-   }
- 
-   if (provider.contactEmail) {
-     cy.get("#provider_main_contact_attributes_email")
-       .clear({ force: true })
-       .type(provider.contactEmail);
-   }
- 
-   if (provider.publicContactsEmail) {
-     cy.get("#provider_public_contacts_attributes_0_email")
-       .clear({ force: true })
-       .type(provider.publicContactsEmail);
-   }
+  if (provider.basicName) {
+    cy.get("#provider_name")
+      .clear({ force: true })
+      .type(provider.basicName);
+  }
 
-   cy.get("#admins-header")
-     .click();
+  if (provider.basicAbbreviation) {
+    cy.get("#provider_abbreviation")
+      .clear({ force: true })
+      .type(provider.basicName);
+  }
 
-    if (provider.adminFirstName) {
-      cy.get("#provider_data_administrators_attributes_0_first_name")
-        .clear({ force: true })
-        .type(provider.adminFirstName);
-    }
+  if (provider.basicWebpage_url) {
+    cy.get("#provider_website")
+      .clear({ force: true })
+      .type(provider.basicWebpage_url);
+  }
+
+  cy.get("#marketing-header")
+    .click();
+
+  if (provider.marketingDescription) {
+    cy.get("#provider_description")
+      .clear()
+      .type(provider.marketingDescription);
+  }
+
+  cy.get("#provider_logo")
+    .attachFile(logo);
+
+  if (provider.marketingMultimedia) {
+    cy.get("#provider_multimedia_0")
+      .clear()
+      .type(provider.marketingMultimedia);
+  }
+
+  cy.get("#classification-header").click();
+
+  if (provider.classificationScientificDomains) {
+    selectItem(provider.classificationScientificDomains, ".provider_scientific_domains");
+  }
+
+  if (provider.classificationTag) {
+    cy.get("#provider_tag_list_0")
+      .clear()
+      .type(provider.classificationTag);
+  }
+
+  cy.get("#location-header")
+    .click()
+
+  if (provider.locationStreet) {
+    cy.get("#provider_street_name_and_number")
+      .clear({ force: true })
+      .type(provider.locationStreet);
+  }
+
+  if (provider.locationPostCode) {
+    cy.get("#provider_postal_code")
+      .clear({ force: true })
+      .type(provider.locationPostCode);
+  }
+
+  if (provider.locationCity) {
+    cy.get("#provider_city")
+      .clear({ force: true })
+      .type(provider.locationCity);
+  }
+
+  if (provider.locationRegion) {
+    cy.get("#provider_region")
+      .clear({ force: true })
+      .type(provider.locationRegion);
+  }
+
+  if (provider.locationCountry) {
+    cy.get("#provider_country")
+      .select(provider.locationCountry);
+  }
+
+  cy.get("#contact-header")
+    .click();
+
+  if (provider.contactFirstname) {
+    cy.get("#provider_main_contact_attributes_first_name")
+      .clear({ force: true })
+      .type(provider.contactFirstname);
+  }
+
+  if (provider.contactLastname) {
+    cy.get("#provider_main_contact_attributes_last_name")
+      .clear({ force: true })
+      .type(provider.contactLastname);
+  }
+
+  if (provider.contactEmail) {
+    cy.get("#provider_main_contact_attributes_email")
+      .clear({ force: true })
+      .type(provider.contactEmail);
+  }
+
+  if (provider.contactPhone) {
+    cy.get("#provider_main_contact_attributes_phone")
+      .clear({ force: true })
+      .type(provider.contactPhone);
+  }
+
+  if (provider.contactPosition) {
+    cy.get("#provider_main_contact_attributes_position")
+      .clear({ force: true })
+      .type(provider.contactPosition);
+  }
+
+  if (provider.publicContactsFirstName) {
+    cy.get("#provider_public_contacts_attributes_0_first_name")
+      .clear({ force: true })
+      .type(provider.publicContactsFirstName);
+  }
+
+  if (provider.publicContactsLastName) {
+    cy.get("#provider_public_contacts_attributes_0_last_name")
+      .clear({ force: true })
+      .type(provider.publicContactsLastName);
+  }
+
+  if (provider.publicContactsEmail) {
+    cy.get("#provider_public_contacts_attributes_0_email")
+      .clear({ force: true })
+      .type(provider.publicContactsEmail);
+  }
+
+  if (provider.publicContactsPhone) {
+    cy.get("#provider_public_contacts_attributes_0_phone")
+      .clear({ force: true })
+      .type(provider.publicContactsPhone);
+  }
+
+  if (provider.publicContactsPosition) {
+    cy.get("#provider_public_contacts_attributes_0_position")
+      .clear({ force: true })
+      .type(provider.publicContactsPosition);
+  }
+
+  cy.get("#maturity-header")
+    .click();
   
-    if (provider.adminLastName) {
-      cy.get("#provider_data_administrators_attributes_0_last_name")
-        .clear({ force: true })
-        .type(provider.adminLastName);
-    }
+  if (provider.maturityProviderLifeCycleStatus) {
+    selectItem(provider.maturityProviderLifeCycleStatus, ".provider_provider_life_cycle_status");
+  }
   
-    if (provider.adminEmail) {
-      cy.get("#provider_data_administrators_attributes_0_email")
-        .clear({ force: true })
-        .type(provider.adminEmail);
-    }
- });
+  if (provider.maturityCertifications) {
+    cy.get("#provider_certifications_0")
+      .clear({ force: true })
+      .type(provider.maturityCertifications);
+  }
+
+  cy.get("#other-header")
+    .click();
+
+  if (provider.otherHostingLegalEntity) {
+    cy.get("#provider_hosting_legal_entity")
+      .clear({ force: true })
+      .type(provider.otherHostingLegalEntity);
+  }
+
+  if (provider.otherParticipatingCountries) {
+    selectItem(provider.otherParticipatingCountries, ".provider_participating_countries");
+  }
+
+  if (provider.otherAffiliations) {
+    cy.get("#provider_affiliations_0")
+      .clear({ force: true })
+      .type(provider.otherAffiliations);
+  }
+
+  if (provider.otherNetworks) {
+    selectItem(provider.otherNetworks, ".provider_networks");
+  }
+
+  if (provider.otherStructureTypes) {
+    selectItem(provider.otherStructureTypes, ".provider_structure_types");
+  }
+
+  if (provider.otherESFRIDomains) {
+    selectItem(provider.otherESFRIDomains, ".provider_esfri_domains");
+  }
+
+  if (provider.otherESFRIType) {
+    selectItem(provider.otherESFRIType, ".provider_esfri_type");
+  }
+
+  if (provider.otherMerilScientificDomains) {
+    selectItem(provider.otherMerilScientificDomains, ".provider_meril_scientific_domains");
+  }
+
+  if (provider.otherAreasOfActivity) {
+    selectItem(provider.otherAreasOfActivity, ".provider_areas_of_activity");
+  }
+
+  if (provider.otherSocietalGrandChallenges) {
+    selectItem(provider.otherSocietalGrandChallenges, ".provider_societal_grand_challenges");
+  }
+
+  if (provider.otherNationalRoadmaps) {
+    cy.get("#provider_national_roadmaps_0")
+      .clear({ force: true })
+      .type(provider.otherNationalRoadmaps);
+  }
+
+  cy.get("#admins-header")
+    .click();
+
+  if (provider.adminFirstName) {
+    cy.get("#provider_data_administrators_attributes_0_first_name")
+      .clear({ force: true })
+      .type(provider.adminFirstName);
+  }
+
+  if (provider.adminLastName) {
+    cy.get("#provider_data_administrators_attributes_0_last_name")
+      .clear({ force: true })
+      .type(provider.adminLastName);
+  }
+
+  if (provider.adminEmail) {
+    cy.get("#provider_data_administrators_attributes_0_email")
+      .clear({ force: true })
+      .type(provider.adminEmail);
+  }
+});
