5a874d123721fac4103c0daf3804317fb4e8f49b
Marcelina <marcelinasiwecka@gmail.com>
2022-02-09 08:09:06 +0100

    Add e2e tests for draft and created by fully form providers
    Add test for create provider where all the fields are filled,
    Add test for checking visibility draft provider in MP na BO

--- test/system/cypress/factories/provider.factory.ts
+++ test/system/cypress/factories/provider.factory.ts
@@ -5,7 +5,6 @@ export class IProviders {
     basicAbbreviation: string;
     basicWebpage_url: string;
     marketingDescription: string;
-    marketingMultimedia: string;
     classificationScientificDomains: string[];
     classificationTag: string;
     locationStreet:string;
@@ -27,7 +26,6 @@ export const ProvidersFactory = {
         basicAbbreviation: Utilities.getUUID4(),
         basicWebpage_url: 'https://example.org/',
         marketingDescription: Utilities.getRandomString(8).toLowerCase(),
-        marketingMultimedia: "https://example.org/",
         classificationScientificDomains: ["Agricultural Sciences ⇒ Agricultural Biotechnology"],
         classificationTag: Utilities.getRandomString(8).toLowerCase(),
         locationStreet:Utilities.getRandomString(8).toLowerCase(),
@@ -40,4 +38,70 @@ export const ProvidersFactory = {
         publicContactsEmail:Utilities.getRandomEmail(),
         ...args
     })
+};
+
+export class IProvidersExtended extends IProviders {
+  marketingMultimedia: string;
+  classificationScientificDomains: string[];
+  locationRegion:string;
+  contactPhone:string;
+  contactPosition:string;
+  publicContactsFirstName:string;
+  publicContactsLastName:string;
+  publicContactsPhone:string;
+  publicContactsPosition:string;
+  maturityProviderLifeCycleStatus:string[];
+  maturityCertifications:string;
+  otherHostingLegalEntity:string;
+  otherParticipatingCountries:string[];
+  otherAffiliations:string;
+  otherNetworks:string[];
+  otherStructureTypes:string[];
+  otherESFRIDomains:string[];
+  otherESFRIType:string[];
+  otherMerilScientificDomains:string[];
+  otherAreasOfActivity:string[];
+  otherSocietalGrandChallenges:string[];
+  otherNationalRoadmaps:string;
+}
+
+export const ProvidersFactoryExtended = {
+  create: (args: {[field: string]: string} = {}): IProvidersExtended => ({
+      basicName: Utilities.getUUID4(),
+      basicAbbreviation: Utilities.getUUID4(),
+      basicWebpage_url: 'https://example.org/',
+      marketingDescription: Utilities.getRandomString(8).toLowerCase(),
+      marketingMultimedia: 'https://example.org/',
+      classificationScientificDomains: ["Agricultural Sciences ⇒ Agricultural Biotechnology"],
+      classificationTag: Utilities.getRandomString(8).toLowerCase(),
+      locationStreet:Utilities.getRandomString(8).toLowerCase(),
+      locationPostCode:Utilities.getRandomString(8).toLowerCase(),
+      locationCity:Utilities.getRandomString(8).toLowerCase(),
+      locationRegion:Utilities.getRandomString(8).toLowerCase(),
+      locationCountry:"Poland",
+      contactFirstname:Utilities.getRandomString(8).toLowerCase(),
+      contactLastname:Utilities.getRandomString(8).toLowerCase(),
+      contactEmail:Utilities.getRandomEmail(),
+      contactPhone: Utilities.getRandomString(),
+      contactPosition: Utilities.getRandomString(),
+      publicContactsFirstName:Utilities.getRandomString(8).toLowerCase(),
+      publicContactsLastName:Utilities.getRandomString(8).toLowerCase(),
+      publicContactsEmail:Utilities.getRandomEmail(),
+      publicContactsPhone: Utilities.getRandomString(),
+      publicContactsPosition: Utilities.getRandomString(),
+      maturityProviderLifeCycleStatus: ["Operational"],
+      maturityCertifications:Utilities.getRandomString(),
+      otherHostingLegalEntity:Utilities.getRandomString(),
+      otherParticipatingCountries: ["Poland"],
+      otherAffiliations: Utilities.getRandomString(),
+      otherNetworks:["AErosol Robotic NETwork (AERONET)"],
+      otherStructureTypes:["Distributed"],
+      otherESFRIDomains: ["Energy"],
+      otherESFRIType: ["Landmark"],
+      otherMerilScientificDomains:["Biological & Medical Sciences ⇒ Animal Facilities"],
+      otherAreasOfActivity: ["Basic Research"],
+      otherSocietalGrandChallenges:["Transport"],
+      otherNationalRoadmaps:Utilities.getRandomString(),
+      ...args
+  })
 };
