6314a5d53c6c1e09d9ae0aa1051c0e7a1012096c
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2022-02-14 13:42:14 +0100

    [#1809] Remove unused files
    Fix misplaced components in the backoffice

--- app/controllers/providers/details_controller.rb
+++ app/controllers/providers/details_controller.rb
@@ -2,8 +2,6 @@
 
 class Providers::DetailsController < ApplicationController
   def index
-    # @service = Service.friendly.find(params[:service_id])
-    # @related_services = @service.related_services
     @provider = Provider.with_attached_logo.friendly.find(params[:provider_id])
     authorize(@provider, :show?)
   end
--- app/helpers/presentable_helper.rb
+++ app/helpers/presentable_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PresentableHelper
-  def data_for_map_provider(geographical_availabilities)
+  def data_for_map(geographical_availabilities)
     countries = []
     geographical_availabilities.each do |place|
       co = []
@@ -16,17 +16,31 @@ module PresentableHelper
       .map { |c| c == ["el", 1] ? ["gr", 1] : c }
   end
 
-  def data_for_region_provider(countries)
+  def data_for_region(countries)
     countries.append("WW") if any_non_european?(countries) && (countries != ["EO"]) && (countries != ["EU"])
     countries
   end
 
   def any_present?(record, *fields)
-    fields.map { |f| record.send(f) }.any?(&:present?)
+    fields.any? { |f| record.send(f).present? }
   end
 
   def field_tree(record, field)
     parents = record.send(field).map { |f| f.parent.blank? ? f : f.parent }
     parents.map { |parent| [parent.name, (parent.children & record.send(field)).map(&:name)] }.to_h
+  end
+
+  def presentable_logo(object, classes = "align-self-center mr-4 float-left img-responsive", resize = "100x67")
+    if object.logo.attached? && object.logo.variable?
+      image_tag object.logo.variant(resize: resize), class: classes
+    else
+      image_pack_tag("eosc-img.png", size: resize, class: classes)
+    end
+  end
+
+  private
+
+  def any_non_european?(countries)
+    (countries - Country.countries_for_region("Europe").map(&:alpha2)).present?
   end
 end
