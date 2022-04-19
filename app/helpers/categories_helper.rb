d8ceb6a5df9081de605bb2b0afe4b4a8cb96a80d
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2022-04-19 18:47:04 +0200

    [#2618] Cleare hierarchy in vocabularies
    Add subcategory class for children entities
    
    backoffice list view mods
    
    list hierarchy indent image
    
    [#2618] Add depth to the categories css classes
    
    list hierarchy indent image- second level

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,4 +4,10 @@ module CategoriesHelper
   def category_query_params
     params.permit!.reject { |p| %w[action controller category_id utf8].include?(p) }
   end
+
+  def style_indentation_variables(record)
+    level = record&.ancestry_depth
+    "--pl-level: #{(30 * level) + 40}px;
+     --bg-level: #{(36 * level) + 30}px;"
+  end
 end
