e7fd069f08f992039e7fb8fa96cc7d9cae224501
Krzysztof Marszałek <marszalekk@gmail.com>
2022-04-21 09:51:53 +0200

    [#2646] Remove unique constraint from Provider
    remove unique from name and abbreviation on provider model
    remove unique from name on provider table

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -100,8 +100,8 @@ class Provider < ApplicationRecord
     self.status ||= :published
   end
 
-  validates :name, presence: true, uniqueness: true
-  validates :abbreviation, presence: true, uniqueness: true
+  validates :name, presence: true
+  validates :abbreviation, presence: true
   validates :website, presence: true
   validates :description, presence: true
   validates :street_name_and_number, presence: true
