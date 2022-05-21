c25abb825659e433480d40bbf7c97396327a2b4e
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2022-05-21 14:52:23 +0200

    Add `tag_list` to the ESS update payload

--- app/services/service/ess/add.rb
+++ app/services/service/ess/add.rb
@@ -34,6 +34,7 @@ class Service::Ess::Add < ApplicationService
       providers_ss: @service.providers.map(&:name),
       target_users_ss: @service.target_users.map { |target_user| hierarchical_to_s(target_user) },
       platforms_ss: @service.platforms.map(&:name),
+      tags_ss: @service.tag_list,
       order_type_s: @service.order_type,
       geographical_availabilities_ss: @service.geographical_availabilities.map(&:alpha2)
     }
