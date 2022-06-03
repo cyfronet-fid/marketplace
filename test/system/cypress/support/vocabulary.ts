b78c9848b03a68276ae89a9c42c2abc92a0b10bd
Marcelina <marcelinasiwecka@gmail.com>
2022-06-03 08:14:20 +0200

    Add e2e test for vocabularies in backoffice
    Add test for adding, deleting and editing vocabulary  in backoffice

---
+++ test/system/cypress/support/vocabulary.ts
@@ -0,0 +1,39 @@
+/**
+ * Define new commands types for typescript (for autocompletion)
+ */
+ import { IVocabulary } from "../factories/vocabulary.factory";
+ 
+ declare global {
+   namespace Cypress {
+     interface Chainable {
+      
+       fillFormCreateVocabulary(vocabulary: Partial<IVocabulary>): Cypress.Chainable<void>;
+ 
+     }
+   }
+ }
+
+ Cypress.Commands.add("fillFormCreateVocabulary", (vocabulary: IVocabulary) => {
+
+  if (vocabulary.name) {
+    cy.get("[id$=_name]")
+      .clear({ force: true })
+      .type(vocabulary.name);
+  }
+
+  if (vocabulary.description) {
+    cy.get("[id$=description]")
+      .clear({ force: true })
+      .type(vocabulary.description);
+  }
+
+  if (vocabulary.parent) {
+    cy.get("[id$=_parent_id]")
+      .select(vocabulary.parent);
+  }
+
+  if (vocabulary.eid) {
+    cy.get("[id$=_eid]")
+      .select(vocabulary.eid);
+  }
+});
