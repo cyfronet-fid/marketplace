9ed435be866495f18cab181544145f70bf34945b
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2023-08-02 14:29:35 +0200

    [#3042] Add dedicated view for bundle
    Add dedicated view for bundle in the mp
    Add link to bundle page in bundle title
    (in the service page)
    Closes #3042

---
+++ app/controllers/services/bundles_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Services::BundlesController < ApplicationController
+  def show
+    @service = policy_scope(Service).friendly.find(params[:service_id])
+    @bundle = policy_scope(@service.bundles.includes(:main_offer, :offers)).find_by(iid: params[:id])
+    @offers = @bundle.all_offers
+  end
+end
---
+++ app/helpers/bundles_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BundlesHelper
+  BUNDLE_DETAILS_FIELDS = %i[bundle_goals capabilities_of_goals research_steps scientific_domains].freeze
+end
