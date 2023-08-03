5663074aba8eda8bea1bafade37539139a787c45
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2023-08-03 19:11:24 +0200

    [#2970] Capabilities of goals suggestion in bundle form
    Add field to get capabilities of bundle goals suggestions
    Field is visible, when someone choose "Other"
    Closes #2970

--- app/controllers/admin/features_controller.rb
+++ app/controllers/admin/features_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Admin::FeaturesController < Admin::ApplicationController
+  def show
+    @suggestions = Bundle.all.map(&:capability_of_goal_suggestion).reject(&:blank?)
+  end
+
   def enable_modal
     User.find_each { |u| u.update!(show_welcome_popup: true) }
     redirect_to admin_features_path, notice: "Welcome modal enabled for all first logged-in users"
