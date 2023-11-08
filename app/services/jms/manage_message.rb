cyfronet-fid/marketplace-euro-cc

[#3094] Datasources as services subprofiles
Add necessary changes to importers
Add new PID to import
Add path to the new PIDs
Closes #3094

app/services/jms/manage_message.rb
@@ -21,7 +21,7 @@ class Jms::ManageMessage < ApplicationService
     @token = token
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity:
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def call
     log @message
@@ -35,6 +35,7 @@ class Jms::ManageMessage < ApplicationService
     case resource_type
     when "service", "infra_service"
       modified_at = modified_at(resource, "serviceBundle")
+      resource["serviceBundle"]["service"]["ppid"] = fetch_ppid(resource["serviceBundle"])
       if resource["serviceBundle"]["service"]["id"].split(".").size != 3
         raise WrongIdError, resource["serviceBundle"]["service"]["id"]
       end
@@ -51,6 +52,7 @@ class Jms::ManageMessage < ApplicationService
       end
     when "provider"
       modified_at = modified_at(resource, "providerBundle")
+      resource["providerBundle"]["provider"]["ppid"] = fetch_ppid(resource["providerBundle"])
       if resource["providerBundle"]["provider"]["id"].split(".").size != 2
         raise WrongIdError, resource["providerBundle"]["provider"]["id"]
       end
@@ -75,21 +77,17 @@ class Jms::ManageMessage < ApplicationService
         )
       end
     when "datasource"
+      hash = resource&.dig("datasourceBundle", "datasource")
       modified_at = modified_at(resource, "datasourceBundle")
-      if resource["datasourceBundle"]["datasource"]["id"].split(".").size != 3
-        raise WrongIdError, resource["datasourceBundle"]["datasource"]["id"]
-      end
+      raise WrongIdError, hash["serviceId"] if hash["serviceId"].split(".").size != 3
+
       case action
       when "delete"
-        Service::DeleteJob.perform_later(resource["datasourceBundle"]["datasource"]["id"])
+        Datasource::DeleteJob.perform_later(hash["serviceId"])
       when "update", "create"
-        Datasource::PcCreateOrUpdateJob.perform_later(
-          resource["datasourceBundle"]["datasource"].merge(resource["datasourceBundle"]["resourceExtras"] || {}),
-          resource["datasourceBundle"]["active"] && !resource["datasourceBundle"]["suspended"],
-          @eosc_registry_base_url,
-          @token,
-          modified_at
-        )
+        is_active = resource["datasourceBundle"]["active"] && !resource["datasourceBundle"]["suspended"]
+        hash["status"] = is_active ? "published" : "draft"
+        Datasource::PcCreateOrUpdateJob.perform_later(hash, is_active, modified_at)
       end
     else
       raise WrongMessageError
@@ -112,6 +110,12 @@ class Jms::ManageMessage < ApplicationService
 
   def log(msg)
     @logger.info(msg)
+  end
+
+  def fetch_ppid(resource)
+    candidate = [resource&.dig("identifiers", "alternativeIdentifiers", "alternativeIdentifier")]
+    candidate = candidate.blank? ? "" : candidate&.find { |id| id["type"] == "PID" }
+    candidate.blank? ? "" : candidate["value"]
   end
 
   def warn(msg)

cyfronet-fid/marketplace

app/services/jms/manage_message.rb
@@ -21,7 +21,7 @@ class Jms::ManageMessage < ApplicationService
     @token = token
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity:
+  # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
 
   def call
     log @message
@@ -40,7 +40,7 @@ class Jms::ManageMessage < ApplicationService
       end
       if action != "delete" && resource["serviceBundle"]["service"]
         Service::PcCreateOrUpdateJob.perform_later(
-          resource["serviceBundle"]["service"].merge(resource["serviceBundle"]["resourceExtras"] || {}),
+          resource["serviceBundle"]["service"].merge(resource_extras(resource["serviceBundle"])),
           @eosc_registry_base_url,
           resource["serviceBundle"]["active"] && !resource["serviceBundle"]["suspended"],
           modified_at,
@@ -75,21 +75,14 @@ class Jms::ManageMessage < ApplicationService
         )
       end
     when "datasource"
+      hash = resource&.dig("datasourceBundle", "datasource")
       modified_at = modified_at(resource, "datasourceBundle")
-      if resource["datasourceBundle"]["datasource"]["id"].split(".").size != 3
-        raise WrongIdError, resource["datasourceBundle"]["datasource"]["id"]
-      end
-      case action
-      when "delete"
-        Service::DeleteJob.perform_later(resource["datasourceBundle"]["datasource"]["id"])
-      when "update", "create"
-        Datasource::PcCreateOrUpdateJob.perform_later(
-          resource["datasourceBundle"]["datasource"].merge(resource["datasourceBundle"]["resourceExtras"] || {}),
-          resource["datasourceBundle"]["active"] && !resource["datasourceBundle"]["suspended"],
-          @eosc_registry_base_url,
-          @token,
-          modified_at
-        )
+      raise WrongIdError, hash["id"] if hash["id"].split(".").size != 3
+
+      if action != "delete" && resource["datasourceBundle"]["datasource"]
+        Datasource::PcCreateOrUpdateJob.perform_later(hash, modified_at)
+      elsif action == "delete"
+        Datasource::DeleteJob.perform_later(hash["id"])
       end
     else
       raise WrongMessageError
@@ -108,6 +101,10 @@ class Jms::ManageMessage < ApplicationService
   def modified_at(resource, resource_type)
     metadata = resource[resource_type]["metadata"]
     Time.at(metadata["modifiedAt"].to_i&./ 1000)
+  end
+
+  def resource_extras(resource)
+    resource.key?("resourceExtras") ? resource["resourceExtras"] : {}
   end
 
   def log(msg)