24f21074e78cd8d6c342188e4bb323ebb9b05c6a
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2023-11-15 15:23:24 +0100

    feat:[#3131] Add new fields to the providers and services
    Add `service_category`
    Change name from `research_step` to `marketplace_location`
    Add alternative_identifiers to providers and services
    
    Closes #3131

--- app/jobs/datasource/pc_create_or_update_job.rb
+++ app/jobs/datasource/pc_create_or_update_job.rb
@@ -5,7 +5,7 @@ class Datasource::PcCreateOrUpdateJob < ApplicationJob
 
   rescue_from(Errno::ECONNREFUSED) { |exception| raise exception }
 
-  def perform(datasource, modified_at)
-    Datasource::PcCreateOrUpdate.new(datasource, modified_at).call
+  def perform(datasource, is_active)
+    Datasource::PcCreateOrUpdate.new(datasource, is_active).call
   end
 end
