7b90336ac8bf229b9a5c5c11393b813acadede76
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2023-12-07 12:56:53 +0100

    fix:[] Render default logo for services which not have it

--- app/controllers/providers/logos_controller.rb
+++ app/controllers/providers/logos_controller.rb
@@ -4,6 +4,10 @@ class Providers::LogosController < ApplicationController
   def show
     @provider = Provider.with_attached_logo.friendly.find(params[:provider_id])
     authorize(@provider)
-    redirect_to @provider.logo.variant(resize: "84x84"), allow_other_host: false
+    if @provider.logo.attached? && @provider.logo.variable?
+      redirect_to @provider.logo.variant(resize: "84x84"), allow_other_host: false
+    else
+      redirect_to ImageHelper::DEFAULT_PROVIDER_LOGO_PATH
+    end
   end
 end
