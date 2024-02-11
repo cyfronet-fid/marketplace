7268f31a7e6b39e13a7a1b1bea1610b218641122
jzbozien <j.zbozien@gmail.com>
2024-02-11 13:27:56 +0100

    [#3179] Catalogues styling

--- app/controllers/catalogues_controller.rb
+++ app/controllers/catalogues_controller.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class CataloguesController < ApplicationController
+  def index
+    @catalogues = policy_scope(Catalogue).order(:name)
+  end
+
   def show
-    @catalogue = Catalogue.friendly.find(params[:id])
+    @catalogue = Catalogue.with_attached_logo.friendly.find(params[:id])
+    authorize(@catalogue)
   end
 end
