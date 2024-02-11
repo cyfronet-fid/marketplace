c09c2cdae8e13a0b7f264c0304b8ec43ff7786fc
jzbozien <j.zbozien@gmail.com>
2024-02-11 13:27:56 +0100

    [#3179] Catalogues styling

---
+++ app/policies/catalogue_policy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CataloguePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(status: %i[published unverified errored])
+    end
+  end
+
+  def show?
+    has_permission = !record.deleted? && !record.draft?
+    raise ActiveRecord::RecordNotFound unless has_permission
+    true
+  end
+end
