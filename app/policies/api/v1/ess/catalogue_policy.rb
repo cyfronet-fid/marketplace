6a917bde5c6060b4a5c342f5606db17994c8e43e
Patryk Wójtowicz <patryk.zbigniew.wojtowicz@gmail.com>
2024-02-19 11:32:14 +0100

    [#3221] Remove EOSC Catalogue from ESS API

--- app/policies/api/v1/ess/catalogue_policy.rb
+++ app/policies/api/v1/ess/catalogue_policy.rb
@@ -3,7 +3,12 @@
 class Api::V1::Ess::CataloguePolicy < Api::V1::EssPolicy
   class Scope < Scope
     def resolve
-      scope.where(status: "published")
+      scope.where(status: "published").where.not(name: "EOSC") # https://github.com/cyfronet-fid/marketplace/issues/3221
     end
+  end
+
+  def show?
+    # https://github.com/cyfronet-fid/marketplace/issues/3221
+    super && record.name != "EOSC"
   end
 end
