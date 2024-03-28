a27783c6438102f644506673ba3b1a4f7e1e623e
Maria <maria77julia@gmail.com>
2024-03-28 18:40:57 +0100

    namespace

--- app/models/language.rb
+++
@@ -1,9 +0,0 @@
-require 'languages'
-
-class Language
-    class << self
-        def living
-        Languages.living.sort_by(&:name)
-        end
-    end
-end
--- app/models/main_title.rb
+++
@@ -1,8 +0,0 @@
-class MainTitle < Title
-    after_initialize :set_type 
-
-    def set_type
-        self.title_type = 'primary'
-    end
-end
-  
