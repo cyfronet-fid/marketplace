38de567e33f0d621bacc0988d1cdd1d706661318
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-04-08 03:12:18 +0200

    fix:[#3254] Fixes for offers feature
    Fixes:
    - Disable and gray out empty choices fields
    - Fix button tag to "Own Ordering System"
    - Fix default parameters' units
    - Add links to the provider's guide and helpdesk
    - Change order of the offer's categories
    - Add tooltips to parameters
    
    Closes #3254

--- app/javascript/app/choices.js
+++ app/javascript/app/choices.js
@@ -5,12 +5,6 @@ export default function initChoices(scope = document) {
   // because the Choices lib will change the "data-choice" value from "true" to "active" after application.
   // This makes this function idempotent.
   const elements = Array.from(scope.querySelectorAll('[data-choice="true"]:not([data-render=false])'));
-  console.log("--------------------");
-  console.log(elements);
-  console.log("--------------------");
-  elements.forEach((e) => {
-    console.log(!e.hasAttribute("data-render"));
-  });
 
   elements.forEach(function (element) {
     new Choices(element, {
