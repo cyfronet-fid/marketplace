f82bbde84cb2dc8f372ecac9ca46ea1036e9c8d5
Maria <maria77julia@gmail.com>
2024-04-24 16:06:33 +0200

    adjust serializer to changed schema

--- app/serializers/api/v1/raid/raid_project_serializer.rb
+++ app/serializers/api/v1/raid/raid_project_serializer.rb
@@ -17,7 +17,7 @@ class Api::V1::Raid::RaidProjectSerializer < ActiveModel::Serializer
 
   def identifier
     {
-        "id": "http://raid.local/10.82841/619abd9d",
+        "id": nil,
         "schemaUri": "https://raid.org/",
         "registrationAgency": {
             "id": "https://ror.org/038sjwq14",
@@ -33,6 +33,8 @@ class Api::V1::Raid::RaidProjectSerializer < ActiveModel::Serializer
         "version": 8
     }
   end
+
+
 
   def date
     {
@@ -60,7 +62,7 @@ class Api::V1::Raid::RaidProjectSerializer < ActiveModel::Serializer
     {
       id: "#{id_schema}#{c.pid}",
       schemaUri: id_schema,
-      position: [serialize_position(c.position.pid, "contributor", "position")],
+      position: [serialize_contributor_position(c.position)],
       role: c_roles,
       leader: c.leader,
       contact: c.contact
@@ -88,19 +90,24 @@ end
     {
       id: o.pid,
       schemaUri: "https://ror.org/",
-      role: [serialize_position(o.position.pid, "organisation", "role")]
-  }
-end
+      role: [serialize_organisation_role(o.position)]
+    }
+  end
 
-def serialize_position(pid, obj, _type)  
-  {
-    schemaUri: "https://github.com/au-research/raid-metadata/tree/main/scheme/#{obj}/#{_type}/v1/",
-    id: "https://github.com/au-research/raid-metadata/blob/main/scheme/#{obj}/#{_type}/v1/#{pid}.json",
-    startDate: "2024-03-18",
-    endDate: "2024-03-30"
-  }
-end
+  def serialize_contributor_position(position)
+    serialized = get_contributor_position(position.pid)
+    serialized[:startDate] = position.start_date
+    serialized[:endDate] = position.end_date
+    serialized
+  end
 
+  def serialize_organisation_role(role)
+    serialized = get_organisation_role(role.pid)
+    p serialized
+    serialized[:startDate] = role.start_date
+    serialized[:endDate] = role.end_date
+    serialized
+  end
 
   def title
     titles = []
@@ -111,48 +118,137 @@ end
     titles
   end
 
-def serialize_title(t)
-  {
-    text: t.text,
-    type:  get_type("title", t.title_type),
-    startDate: t.start_date,
-    endDate: t.end_date,
-    language: get_language(t.language)
-  }
-end
+  def serialize_title(t)
+    {
+      text: t.text,
+      type:  get_title_type(t.title_type),
+      startDate: t.start_date,
+      endDate: t.end_date,
+      language: get_language(t.language)
+    }
+    end
 
 
 
-def serialize_description(d)
-  {
+  def serialize_description(d)
+    {
     text: d.text,
-    type:  get_type("description", d.description_type),
+    type:  get_description_type(d.description_type),
     language: get_language(d.language)
-  }
-end
-
-def access
-  {
-    type:  get_type("access", object.raid_access.access_type),
-    statement: {
-        text: object.raid_access.statement_text,
-        language: get_language(object.raid_access.statement_lang)
-    },
-    embargoExpiry: object.raid_access.embargo_expiry
-}
-end
-
-def get_language(language)
-  {
-        id: language,
-        schemaUri: "https://iso639-3.sil.org"
     }
   end
 
-def get_type(obj, obj_type)
-  {
-        id: "https://github.com/au-research/raid-metadata/blob/main/scheme/#{obj}/type/v1/#{obj_type}.json",
-        schemaUri: "https://github.com/au-research/raid-metadata/tree/main/scheme/#{obj}/type/v1/"
+  def access
+    if object.raid_access.access_type == "open" && object.raid_access.statement_text.empty?
+      return
+    end
+    {
+      type:  get_access_type(object.raid_access.access_type),
+     statement: {
+        text: object.raid_access.statement_text,
+        language: get_language(object.raid_access.statement_lang)
+      },
+      embargoExpiry: object.raid_access.embargo_expiry  
+  }
+  end
+
+  def get_language(language)
+    {
+        id: language,
+        schemaUri: "https://www.iso.org/standard/74575.html"
+      }
+  end
+
+  def get_access_type(access_type)
+    access_type_map = {
+      open: "c_abf2",
+      embargoed: "c_f1cf",
+    }
+    a_type = access_type_map[access_type.to_sym]
+    if a_type.nil?
+      p "Unpermitted access type"  # TODO error handling
+      return
+    end
+    {
+      id: "https://vocabularies.coar-repositories.org/access_rights/#{a_type}/",
+      schemaUri: "https://vocabularies.coar-repositories.org/access_rights/"
+    }
+  end
+
+  def get_title_type(title_type)
+    schema = "https://vocabulary.raid.org/title.type.schema"
+    type_pid = "376"
+    title_type_map = {
+      primary: "5",
+      alternative: "4"
+    }
+    t_type = title_type_map[title_type.to_sym]
+    if t_type.nil?
+      p "Unpermitted title type"  # TODO error handling
+      return
+    end
+    {
+      id: "#{schema}/#{t_type}",
+      schemaUri: "#{schema}/#{type_pid}"
+    }
+  end
+
+  def get_description_type(description_type)
+    schema = "https://vocabulary.raid.org/description.type.schema"
+    type_pid = "320"
+    description_type_map = {
+      "primary": "318",
+      "alternative": "319"
+    }
+    d_type = description_type_map[description_type.to_sym]
+    if d_type.nil?
+      p "Unpermitted description type"  # TODO error handling
+      return
+    end
+    {
+      id: "#{schema}/#{d_type}",
+      schemaUri: "#{schema}/#{type_pid}"
+    }
+  end
+
+  def get_organisation_role(pid)
+    schema = "https://vocabulary.raid.org/organisation.role.schema"
+    organisation_roles_pid = "359"
+    organisation_roles_map = {
+      "lead-research-organisation": "182",
+      "contractor": "185",
+      "other-organisation": "188",
+      "other-research-organisation": "183",
+      "partner-organisation": "184"
+    }
+    o_role = organisation_roles_map[pid.to_sym]
+    if o_role.nil?
+      p "Unpermitted organisation role"  # TODO error handling
+      return
+    end
+    {
+      id: "#{schema}/#{o_role}",
+      schemaUri: "#{schema}/#{organisation_roles_pid}"
+    }
+   
+end
+
+  def get_contributor_position(pid)
+    schema = "https://vocabulary.raid.org/contributor.position.schema"
+    contributor_position_pid = "305"
+    contributor_positions_map =  {
+      "principal-investigator": "307",
+      "co-investigator": "308",
+      "other-participant": "311"
+    }
+    c_position = contributor_positions_map[pid.to_sym]
+    if c_position.nil?
+      p "Unpermitted contributor position"  # TODO error handling
+      return
+    end
+    {
+      id: "#{schema}/#{c_position}",
+      schemaUri: "#{schema}/#{contributor_position_pid}"
     }
   end
 end
