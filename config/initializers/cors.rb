e3e27938be80114649b9d0cde1f08bede8e28bd7
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-05-23 10:50:58 +0200

    Remove unused and unnecessary code
    Allow only known origins in CORS configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins %r(/\Alocalhost:\d{1024,65535}\z/),
+            %r{/\Ahttps://search-\d{1,15}\.docker-fid\.grid\.cyf-kr\.edu\.pl\z/},
+            %r(/\Ahttps://marketplace-\d{1,15}\.docker-fid\.grid\.cyf-kr\.edu\.pl/\z/),
+            "https://aai.eosc-portal.eu/",
+            "https://beta.marketplace.eosc-portal.eu/",
+            "https://beta.search.marketplace.eosc-portal.eu/"
+
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete, :options, :head]
   end
 end
