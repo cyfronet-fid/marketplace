7ec2302da061b95e91af8c35ad213ddaf0bc05c6
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-06-26 17:58:45 +0200

    feat:[#5] Add delete action for services
    Add delete button
    in the service backoffice view
    
    Closes #5

--- spec/services/service/destroy_spec.rb
+++ spec/services/service/destroy_spec.rb
@@ -3,11 +3,11 @@
 require "rails_helper"
 
 RSpec.describe Service::Destroy, backend: true do
-  it "removes service from db" do
+  it "changes service status to deleted in the db" do
     service = create(:service)
 
     described_class.new(service).call
 
-    expect(Service.find_by(id: service.id)).to be_nil
+    expect(Service.find_by(id: service.id)&.status).to eq("deleted")
   end
 end
