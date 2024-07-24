deb58d8023a0d6130c9bee32a568bd2fc8633b68
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-07-24 14:21:50 +0200

    fix:[#HOTFIX] Collapse all filters by default

--- app/models/service/search.rb
+++ app/models/service/search.rb
@@ -25,7 +25,7 @@ module Service::Search
     {
       service_id: id,
       name: name,
-      sort_name: name.downcase,
+      sort_name: name&.downcase,
       tagline: tagline,
       description: description,
       status: status,
