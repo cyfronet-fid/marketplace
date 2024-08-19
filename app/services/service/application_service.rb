e191bc98756badcc6d5a8f8b06de23007df9c3c7
Maria <maria77julia@gmail.com>
2024-08-19 11:05:23 +0200

    draft bundle if offer not publish

--- app/services/service/application_service.rb
+++ app/services/service/application_service.rb
@@ -5,4 +5,15 @@ class Service::ApplicationService < ApplicationService
     super()
     @service = service
   end
+
+  def unbundle_and_notify!
+    @service
+      .offers
+      .map(&:bundles)
+      .flatten
+      .uniq
+      .each do |bundle|
+        Bundle::Update.call(bundle, { offer_ids: (bundle.offer_ids - @service.offer_ids).to_a }, external_update: true)
+      end
+  end
 end
