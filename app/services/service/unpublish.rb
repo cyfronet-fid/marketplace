8f0bb331ec45cb12915cea8dee492756a96d6bf1
Maria <maria77julia@gmail.com>
2024-08-19 11:05:23 +0200

    draft bundle if offer not publish

--- app/services/service/unpublish.rb
+++ app/services/service/unpublish.rb
@@ -7,17 +7,4 @@ class Service::Unpublish < Service::ApplicationService
     unbundle_and_notify! if result && public_before
     result
   end
-
-  private
-
-  def unbundle_and_notify!
-    @service
-      .offers
-      .map { |o| [o, o.bundles] }
-      .each do |offer, bundles|
-        bundles.each do |bundle|
-          Bundle::Update.call(bundle, { offers: bundle.offers.to_a.reject { |o| o == offer } }, external_update: true)
-        end
-      end
-  end
 end
