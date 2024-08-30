7b3aa5faf9c3a95a53aa95369bbe6119e83c50f5
Pawel Gorczyca <p.gorczyca@cyfronet.pl>
2024-08-30 10:23:35 +0200

    fix:[#95] Fix adding a new offer
    Fix policy method for displaying `Publish`
    action in offers. Add persistence check
    
    Closes #95

---
+++ app/policies/backoffice/orderable_policy.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class Backoffice::OrderablePolicy < Backoffice::ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user.service_portfolio_manager? || user.data_administrator?
+        scope.where.not(status: Statusable::INVISIBLE_STATUSES)
+      else
+        scope.none
+      end
+    end
+  end
+
+  def new?
+    managed? && !service_deleted?
+  end
+
+  def create?
+    managed? && !service_deleted?
+  end
+
+  def edit?
+    managed? && !service_deleted?
+  end
+
+  def update?
+    managed? && !service_deleted?
+  end
+
+  def destroy?
+    status_change? && orderless? && !service_deleted?
+  end
+
+  def delete?
+    status_change? && !service_deleted?
+  end
+
+  def draft?
+    status_change? && record.published?
+  end
+
+  def publish?
+    status_change? && !record.published? && !service_deleted?
+  end
+
+  private
+
+  def status_change?
+    managed? && record.persisted?
+  end
+
+  def managed?
+    service_portfolio_manager? || record.service.owned_by?(user)
+  end
+
+  def orderless?
+    record.project_items.count.zero?
+  end
+
+  def service_deleted?
+    record.service.deleted?
+  end
+end
