f63180cb57bc90e21baf4c8a125b0de86dc8890b
Maria <maria77julia@gmail.com>
2024-09-03 11:16:57 +0200

    copy status action to edit view

---
+++ app/components/presentable/status_actions_component.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class Presentable::StatusActionsComponent < ApplicationComponent
+  include FormsHelper
+  def initialize(object:, publish: false, unpublish: false, suspend: false, destroy: false)
+    super()
+    @object = object
+    @object_type = object.class.name.downcase
+    @publish = publish
+    @unpublish = unpublish
+    @suspend = suspend
+    @destroy = destroy
+  end
+
+  def suspend_path
+    case @object
+    when Service
+      backoffice_service_draft_path(@object, suspend: true)
+    when Provider
+      backoffice_provider_unpublish_path(@object, suspend: true)
+    when Catalogue
+      backoffice_catalogue_unpublish_path(@object, suspend: true)
+    end
+  end
+
+  def unpublish_path
+    case @object
+    when Service
+      backoffice_service_draft_path(@object)
+    when Provider
+      backoffice_provider_unpublish_path(@object)
+    when Catalogue
+      backoffice_catalogue_unpublish_path(@object)
+    end
+  end
+end
