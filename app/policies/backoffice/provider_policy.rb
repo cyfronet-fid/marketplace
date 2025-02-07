81c3d98ebdfe23b7e2b144520ae2d7a15f2dd592
Paweł Gorczyca <goreck888@gmail.com>
2025-02-07 13:23:30 +0100

    feat:[#132] Add panel for accept providers
    Add panel with approval requests
    in the backoffice providers view
    Styling for approval modal
    and accept providers panel
    
    Closes #132

--- app/policies/backoffice/provider_policy.rb
+++ app/policies/backoffice/provider_policy.rb
@@ -105,4 +105,8 @@ class Backoffice::ProviderPolicy < Backoffice::ApplicationPolicy
   def access?
     coordinator? || (record&.owned_by?(user) && record&.approval_requests&.none?(&:published?))
   end
+
+  def access?
+    coordinator? || (record&.owned_by?(user) && record&.approval_requests&.none?(&:published?))
+  end
 end
