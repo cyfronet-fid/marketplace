44d6e3bd391fa890454267347762d51b66f38f4e
Paweł Gorczyca <goreck888@gmail.com>
2025-05-28 10:43:04 +0200

    fix: Disable GA and Monitoring endpoint
    Disable Google Analytics
    and Monitoring Endpoint
    to get quick response on services

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,7 @@ module Mp
         "https://beta.providers.eosc-portal.eu"
       end
     config.google_api_key_path = ENV.fetch("GOOGLE_AUTH_KEY_FILEPATH", "config/google_api_key.json")
+    config.monitoring_data_enabled = ActiveModel::Type::Boolean.new.cast(ENV.fetch("MONITORING_DATA_ENABLED", false))
     config.monitoring_data_host = ENV.fetch("MONITORING_DATA_URL", "https://api.devel.argo.grnet.gr/api")
     config.monitoring_data_token = ENV.fetch("MONITORING_DATA_TOKEN",
                                              Rails.application.credentials.monitoring_data[:access_token])
@@ -112,7 +113,7 @@ module Mp
     config.eosc_helpdesk_form_link = ENV.fetch("EOSC_HELPDESK_FORM_URL", "https://helpdesk.sandbox.eosc-beyond.eu/assets/form/form.js")
 
     config.enable_external_search = ActiveModel::Type::Boolean.new.cast(ENV.fetch("MP_ENABLE_EXTERNAL_SEARCH", false))
-
+    config.analytics_enabled = ActiveModel::Type::Boolean.new.cast(ENV.fetch("ANALYTICS_ENABLED", false))
     config.whitelabel = ENV.fetch("MP_WHITELABEL", false)
   end
 end
