e1aaa2f776af31fceddacd352c8c9396ec5b3f90
Pawel Gorczyca <goreck888@gmail.com>
2025-07-23 13:31:51 +0200

    feat:[3480] Apply new tab feature to header
    Add new Provider tab to the header
    dropdown menu
    
    Closes #3480

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,15 +55,28 @@ module ApplicationHelper
   end
 
   def eosc_commons_profile_links
-    links = []
+    []
+    # links = []
 
     # if show_administrative_sections?
     #   .border-top
+    # if policy(%i[backoffice backoffice]).show?
+    #   links.push({ href: backoffice_path, caption: _("Backoffice"), "data-e2e": "backoffice" })
+    # end
+    # links.push({ href: admin_path, caption: _("Admin") }) if policy(%i[admin admin]).show?
+    # links.to_json
+  end
+
+  def eosc_commons_custom_tabs
+    links = []
+
     if policy(%i[backoffice backoffice]).show?
       links.push({ href: backoffice_path, caption: _("Backoffice"), "data-e2e": "backoffice" })
     end
     links.push({ href: admin_path, caption: _("Admin") }) if policy(%i[admin admin]).show?
-    links.to_json
+    links.push({ href: api_docs_path, caption: "Marketplace API" })
+
+    [{ id: "provider", name: "Provider", links: links }].to_json
   end
 
   def meta_og_title_content
