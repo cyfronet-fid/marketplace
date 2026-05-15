d7fc5b12f2f29e574b0678502a48695190aec384
szymon figura <szymon.figura@cyfronet.pl>
2026-05-15 15:08:22 +0200

    fix: change federated search base api url

--- config/application.rb
+++ config/application.rb
@@ -111,7 +111,7 @@ module Mp
     config.bos_api_key = ENV.fetch("BOS_API_KEY", "")
     config.bos_enabled = ActiveModel::Type::Boolean.new.cast(ENV.fetch("BOS_ENABLED", false))
 
-    config.federation_api_base_url = ENV.fetch("FEDERATION_API_BASE_URL", "http://79.129.71.168/federation/services")
+    config.federation_api_base_url = ENV.fetch("FEDERATION_API_BASE_URL", "http://federatedsearch.service.eosc-beyond.eu/federation/services")
     config.aggregator_type = ENV.fetch("AGGREGATOR_TYPE", "pc")
   end
 end
