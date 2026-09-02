7a2c3b73a08e6b7b384007828143702cbdb4ef8c
Szymon Chodzidlo <szymon.chodzidlo@cyfronet.pl>
2026-09-02 11:10:58 +0200

    refactor(services): [#3744] use a guard clause in save_in_session
    - avoids the long trailing conditional split across multiple lines

--- app/controllers/services/application_controller.rb
+++ app/controllers/services/application_controller.rb
@@ -73,10 +73,9 @@ class Services::ApplicationController < ApplicationController
 
   def save_in_session(step)
     session[session_key] = step.project_item.attributes
-    session[session_key][:bundled_parameters] = step.project_item.bundled_parameters.transform_keys(&:id) if step
-                                                                                                             .project_item
-                                                                                                             .bundled_parameters
-                                                                                                             .present?
+    return if step.project_item.bundled_parameters.blank?
+
+    session[session_key][:bundled_parameters] = step.project_item.bundled_parameters.transform_keys(&:id)
   end
 
   def saved_state
